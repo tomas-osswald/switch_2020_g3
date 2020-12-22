4324d7762627b65c2a43ddba41fe41ebe02d7027
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2020-12-22 18:04:08 +0000

    Added Javadocs. All tests passed

--- src/main/java/switch2020/project/model/Application.java
+++ src/main/java/switch2020/project/model/Application.java
@@ -16,6 +16,12 @@ public class Application {
         this.families.add(family);
     }
 
+    /**
+     * Method to add an EmailAddress object with the passed email address string to the FamilyMember with the passed ID
+     * @param emailToAdd String of the email address to add
+     * @param familyMemberID Integer representing the family member's ID
+     * @return True if email successfully added to the Family Member with the passed ID
+     */
     public boolean addEmail(String emailToAdd, int familyMemberID) {
         return this.families.get(0).addEmail(emailToAdd, familyMemberID);
     }
--- src/main/java/switch2020/project/model/FamilyMember.java
+++ src/main/java/switch2020/project/model/FamilyMember.java
@@ -12,10 +12,19 @@ public class FamilyMember {
         this.familyMemberID = iD;
     }
 
+    /**
+     * @return Int representing the FamilyMember's ID.
+     */
     public int getID() {
         return this.familyMemberID;
     }
 
+    /**
+     * Method to create an EmailAddress object and add it to the ArrayList of EmailAddress objects
+     *
+     * @param emailToAdd String of the email address to add
+     * @return True if the EmailAddress object is successfully created and added to the EmailAddress ArrayList
+     */
     public boolean addEmail(String emailToAdd) {
         if (!isEmailAlreadyPresent(emailToAdd)) {
             EmailAddress newEmail = new EmailAddress(emailToAdd);
@@ -25,6 +34,12 @@ public class FamilyMember {
         return false;
     }
 
+    /**
+     * Method to check if a given email address is already present in the ArrayList of EmailAddress objects
+     *
+     * @param emailToCheck String representing the email address to check if present
+     * @return True if the passed email address is already present
+     */
     private boolean isEmailAlreadyPresent(String emailToCheck) {
         for (EmailAddress email : emails) {
             if (email.getEmail().equalsIgnoreCase(emailToCheck)) {
