5635287695726447f0f14194e53fa89805e9ffb0
Diogo Ribeiro <diogoribeiro@Diogos-MacBook-Pro.local>
2021-01-05 09:42:01 +0000

    create controller

---
+++ src/switch2020/project/controllers/addFamilyMemberController.java
@@ -0,0 +1,18 @@
+package switch2020.project.controllers;
+
+import switch2020.project.model.Application;
+
+public class addFamilyMemberController {
+
+    private Application app;
+
+    public addFamilyMemberController(Application app){
+        this.app.add;
+    }
+
+    private boolean addFamilyMember(/* colocar variaveis que sao necessarias para FamilyMember */){
+
+        return this.app.createFamilyMember(/* colocar variaveis que sao necessarias para FamilyMember */);
+
+    }
+}
