ac8328db181c0c8ce14a547897c4bac7742ad193
MiguelLoureiro <1181945@isep.ipp.pt>
2021-01-05 14:53:01 +0000

    updated

---
+++ src/main/java/switch2020/project/controllers/StandardCategoriesController.java
@@ -0,0 +1,11 @@
+package switch2020.project.controllers;
+import switch2020.project.model.Application;
+
+public class StandardCategoriesController {
+    private Application FFMApplication;
+
+    public Application getFFMApplication() {
+        return FFMApplication;
+    }
+
+}
--- src/main/java/switch2020/project/model/Category.java
+++ src/main/java/switch2020/project/model/Category.java
@@ -1,33 +1,14 @@
 package switch2020.project.model;
 
-import java.util.Scanner;
-
 public class Category {
-    //atributes
     private String cname;
     private int parentNumber;
-    private boolean isStandard;
 
-    //constructor
-
-    public Category(String cname, int parentNumber) {
-        this.cname = cname;
-        this.parentNumber = parentNumber;
-
-        if(cname.length() < 1){
-            try {
-                throw new Exception("Category Name must have at least 1 character");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            else {
-                this.cname = cname;
-            }
-        }
+    public Category(){
+        this.cname = " ";
+        this.parentNumber = -1;
     }
 
+    public Category(String cname, int parentNumber){
+    }
 }
-
-
-
-
