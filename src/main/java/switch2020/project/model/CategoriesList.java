9ae8371a66ba9dea3915dcc16a6d53f8dd1c9c6d
MiguelLoureiro <1181945@isep.ipp.pt>
2021-01-05 14:53:17 +0000

    updated

---
+++ src/main/java/switch2020/project/model/CategoriesList.java
@@ -0,0 +1,15 @@
+package switch2020.project.model;
+import java.util.ArrayList;
+
+public class CategoriesList {
+    private ArrayList<Category> catList;
+
+    public CategoriesList(){
+        this.catList = new ArrayList();
+        }
+
+        public addCat(String cname){
+        Category c;
+
+    }
+}
