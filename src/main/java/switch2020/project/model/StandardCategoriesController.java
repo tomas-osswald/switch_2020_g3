8243f3d7be53e43fc84d0ce8817db615c9bebde3
MiguelLoureiro <1181945@isep.ipp.pt>
2021-01-06 23:38:25 +0000

    correcting merge problems

--- src/main/java/switch2020/project/model/StandardCategoriesController.java
+++ src/main/java/switch2020/project/model/StandardCategoriesController.java
@@ -1,6 +1,11 @@
 package switch2020.project.model;
+import switch2020.project.model.Application;
 
 public class StandardCategoriesController {
     private Application FFMAplication;
 
+    public StandardCategoriesController(Application FFMAplication) {
+        this.FFMAplication = FFMAplication;
+    }
 }
+
