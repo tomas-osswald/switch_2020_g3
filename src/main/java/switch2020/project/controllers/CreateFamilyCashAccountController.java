3814f74eef54555b98f8166467a0404e6654824a
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-01-06 17:36:51 +0000

    code cleanup

--- src/main/java/switch2020/project/controllers/CreateFamilyCashAccountController.java
+++ src/main/java/switch2020/project/controllers/CreateFamilyCashAccountController.java
@@ -9,15 +9,5 @@ public class CreateFamilyCashAccountController {
         this.FFMapp = app;
     }
 
-    /**
-     * Method to create a CashAccount object for the Family with the passed ID
-     *
-     * @param familyID integer identifier of family for which account will be created
-     * @return True if cash account succesfully created for the family with the given ID
-     */
-    public boolean createFamilyCashAccount(int familyID) {
-        return this.FFMapp.createFamilyCashAccount(familyID);
-    }
-
 
 }
