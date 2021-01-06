bfe92505856d4ebe455433008e4e01a82f9ae2b6
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-01-06 16:20:03 +0000

    merge fixes

--- src/main/java/switch2020/project/model/Application.java
+++ src/main/java/switch2020/project/model/Application.java
@@ -1,22 +1,36 @@
 package switch2020.project.model;
 
+import switch2020.project.services.FamilyService;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    //attributes
-    //private ArrayList<Category> categories = new ArrayList();
-    private CategoryService categoryService;
 
+    private CategoryService categoryService = new CategoryService();
+    private FamilyService familyService = new FamilyService();
 
-    //constructors
-    public Application() {
-        this.categoryService = new CategoryService();
+    public Application(){
+
+    }
+
+    public Application(Family family) {
+        this.familyService.addFamily(family);
     }
 
     public CategoryService getCategoryService() {
         return this.categoryService;
     }
 
-
+    /**
+     * Method to add an EmailAddress object with the passed email address string to the FamilyMember with the passed ID
+     *
+     * @param emailToAdd     String of the email address to add
+     * @param familyID       Integer representing the family's ID
+     * @param familyMemberID Integer representing the family member's ID
+     * @return True if email successfully added to the Family Member with the passed ID
+     */
+    public boolean addEmail(String emailToAdd, int familyID, int familyMemberID) {
+        return this.familyService.addEmail(emailToAdd, familyID, familyMemberID);
+    }
 }
