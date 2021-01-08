cab2d1ed878fa985f5940b24c955e775b429a332
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-01-08 12:12:53 +0000

    built constructor for CategoryTree to retrieve the standard categories. Began building the getCategoryTree controller

---
+++ src/main/java/switch2020/project/controllers/GetCategoryTreeController.java
@@ -0,0 +1,26 @@
+package switch2020.project.controllers;
+
+import switch2020.project.model.Application;
+import switch2020.project.model.CategoryTree;
+import switch2020.project.services.CategoryService;
+import switch2020.project.services.FamilyService;
+
+public class GetCategoryTreeController {
+
+    private Application ffmApp;
+
+    public GetCategoryTreeController(Application app) {
+        this.ffmApp = app;
+    }
+
+    public CategoryTree getCategoryTree(int familyID){
+        FamilyService familyService = this.ffmApp.getFamilyService();
+        CategoryService categoryService = this.ffmApp.getCategoryService();
+        CategoryTree categoryTree = new CategoryTree(categoryService);
+
+
+
+        return categoryTree;
+    }
+
+}
--- src/main/java/switch2020/project/model/CategoryTree.java
+++ src/main/java/switch2020/project/model/CategoryTree.java
@@ -1,4 +1,14 @@
 package switch2020.project.model;
 
+import switch2020.project.services.CategoryService;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryTree {
+    List<Category> categoryTree = new ArrayList();
+
+    public CategoryTree(CategoryService service){
+        this.categoryTree.addAll(service.getStandardCategories());
+    }
 }
