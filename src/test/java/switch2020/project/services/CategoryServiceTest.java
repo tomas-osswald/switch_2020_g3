9ce8ba9107e8e978e9c6e1649ab1b3deefff8862
Miguel Loueiro[1181945] <1181945@isep.ipp.pt>
2021-01-11 23:23:07 +0000

    update

--- src/test/java/switch2020/project/services/CategoryServiceTest.java
+++ src/test/java/switch2020/project/services/CategoryServiceTest.java
@@ -1,46 +1,4 @@
-package switch2020.project.services;
-
-import org.junit.jupiter.api.Test;
-import switch2020.project.controllers.GetCategoryTreeController;
-import switch2020.project.model.Application;
-import switch2020.project.model.StandardCategory;
-import switch2020.project.utils.StandardCategoryDTO;
-import switch2020.project.services.CategoryService;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 class CategoryServiceTest {
-
- /*   Application app = new Application();
-    List<StandardCategory> categs = new ArrayList<StandardCategory>();
-    StandardCategory cat1 = new StandardCategory("Casa", 0,1);
-    StandardCategory cat2 = new StandardCategory("Saude", 0,1);
-    StandardCategory cat3 = new StandardCategory("Educacao", 0,1);
-*/
-/*
-    @Test{
-
-    }
-   */
-/* void getStandardCategoriesDTOList() {
-        List<StandardCategory> categs = new ArrayList<StandardCategory>();
-        StandardCategory cat1 = new StandardCategory("Casa", 0,1);
-        StandardCategory cat2 = new StandardCategory("Saude", 0,1);
-        StandardCategory cat3 = new StandardCategory("Educacao", 0,1);
-        Application app = new Application();
-        GetCategoryTreeController novo = new GetCategoryTreeController(app);
-        app.getCategoryService().cloneOfStandardcategoriesList();
-        List<StandardCategory> categs = new ArrayList<StandardCategory>();
-        categs.add(cat1);
-        categs.add(cat2);
-        categs.add(cat3);
-
-        List <StandardCategory> result = app.getCategoryService().cloneOfStandardcategoriesList();
-
-    }*//*
-
-}*/
+  
 }
