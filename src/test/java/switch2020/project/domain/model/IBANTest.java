b52b6aba8d30112b8933b427985c648dcdc795f4
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-01-21 09:28:25 +0000

    Finished tests for IBAN

--- src/test/java/switch2020/project/domain/model/IBANTest.java
+++ src/test/java/switch2020/project/domain/model/IBANTest.java
@@ -5,11 +5,58 @@ import org.junit.jupiter.api.Test;
 
 class IBANTest {
     String validIBAN = "PT50019300001050479170723";
-    String invalidIBAN = "";
+    String spacedValidIBAN = "PT500 1930 0001 0504 7917 0723";
+    String invalidIBAN = "PT50019300001050479270723";
+    String nullIBAN = null;
+    String blankIBAN = "    ";
+    String emptyIBAN = "";
+
 
     @Test
     void testCreatingIBAN() {
         IBAN iban = new IBAN(validIBAN);
         Assertions.assertNotNull(iban);
+    }
+
+    @Test
+    void testCreatingIBANNotThrows() {
+        Assertions.assertDoesNotThrow(() -> {
+            IBAN iban = new IBAN(validIBAN);
+        });
+    }
+
+    @Test
+    void testCreatingIBANSpaced() {
+        Assertions.assertDoesNotThrow(() -> {
+            IBAN iban = new IBAN(spacedValidIBAN);
+        });
+    }
+
+    @Test
+    void testCreatingIBANThrowsInvalidIBAN() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            IBAN iban = new IBAN(invalidIBAN);
+        });
+    }
+
+    @Test
+    void testCreatingIBANThrowsBlankIBAN() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            IBAN iban = new IBAN(blankIBAN);
+        });
+    }
+
+    @Test
+    void testCreatingIBANThrowsNullIBAN() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            IBAN iban = new IBAN(nullIBAN);
+        });
+    }
+
+    @Test
+    void testCreatingIBANThrowsEmptyIBAN() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            IBAN iban = new IBAN(emptyIBAN);
+        });
     }
 }
