6ffd9eb922f601d3a80dfc4e26394640ca7ff545
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-01-22 11:43:26 +0000

    Accounts now store their attributes in an AccountData class

---
+++ src/main/java/switch2020/project/domain/model/AccountData.java
@@ -0,0 +1,40 @@
+package switch2020.project.domain.model;
+
+public class AccountData {
+
+    private double balance = 0;
+    private String description;
+    private int accountID;
+
+
+    public AccountData(double balance, String description, int accountID) {
+        this.balance = balance;
+        this.description = description;
+        this.accountID = accountID;
+    }
+
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+}
