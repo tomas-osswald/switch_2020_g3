f4db799610c4ff05d693f8febceb6ca8e1a0d68e
Miguel Loueiro[1181945] <1181945@isep.ipp.pt>
2021-01-25 18:44:38 +0000

    update in local file only

---
+++ src/test/java/switch2020/project/domain/model/CreditCardAccountTest.java
@@ -0,0 +1,13 @@
+package switch2020.project.domain.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CreditCardAccountTest {
+
+    @Test
+    void atest(){
+
+    }
+  
+}
