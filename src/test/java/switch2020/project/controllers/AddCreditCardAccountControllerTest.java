620a80738fb2fb3d2d9b42b257b961341e9dc0c9
Miguel Loueiro[1181945] <1181945@isep.ipp.pt>
2021-01-26 11:38:11 +0000

    added tests to AccountServiceTest and AddCreditCardAccountControllerTest

--- src/test/java/switch2020/project/controllers/AddCreditCardAccountControllerTest.java
+++ src/test/java/switch2020/project/controllers/AddCreditCardAccountControllerTest.java
@@ -1,77 +1,80 @@
 package switch2020.project.controllers;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import switch2020.project.domain.model.*;
-import switch2020.project.domain.services.AccountService;
+import switch2020.project.domain.model.Application;
+import switch2020.project.domain.model.Family;
+import switch2020.project.domain.model.FamilyMember;
+import switch2020.project.domain.model.Relation;
 import switch2020.project.domain.services.FamilyService;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Date;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class AddCreditCardAccountControllerTest {
 
-   // family1 data
-    int family1ID = 5;
-    String family1Name = "Silva";
-    Family silva = new Family(family1Name, family1ID);
+    Application app = new Application();
+    FamilyService familyService = app.getFamilyService();
 
-    // family1 member1 data
-    String cc1 = "166699209ZY8";
-    String name1 = "Ze";
-    Date date1 = new Date(1970, 1, 2);
-    int numero1 = 931456789;
-    String email1 = "ze90@gmail.com";
-    int nif1 = 231874316;
-    String rua1 = "Rua dos Abracos";
-    String codPostal1 = "4000-011";
-    String local1 = "Porto";
-    String city1 = "Porto";
-    String relacao1 = "pai";
-    Relation relation1 = new Relation(relacao1);
-    boolean admin1 = true;
+    AddCreditCardAccountController addCreditCardAccountController = new AddCreditCardAccountController(app);
 
-    // family1 member2 data
-    String cc2 = "137476450ZX0";
-    String name2 = "Maria";
-    Date date2 = new Date(2001, 6, 12);
-    int numero2 = 938036428;
-    String email2 = "maria303@gmail.com";
-    int nif2 = 299525295;
-    String rua2 = "Rua dos Carqueijais";
-    String codPostal2 = "4000-181";
-    String local2 = "Porto";
+
+    String cc = "000000000ZZ4";
+    String name = "Diogo";
+    Date date = new Date(1990, 8, 26);
+    Integer numero = 919999999;
+    String email = "josediogoccbr@gmail.com";
+    int nif = 212122233;
+    String rua = "Rua Nossa";
+    String codPostal = "4444-555";
+    String local = "Zinde";
+    String city = "Porto";
+    String relacao = "filho";
+    Relation relation = new Relation(relacao);
+    boolean admin = true;
+
+    String id2 = "166699209ZY8";
+    String name2 = "Tony";
+    Date date2 = new Date(1954, 8, 26);
+    int numero2 = 919999998;
+    String email2 = "tony@gmail.com";
+    int nif2 = 212122000;
+    String rua2 = "Rua";
+    String codPostal2 = "4444-556";
+    String local2 = "Gaia";
     String city2 = "Porto";
-    String relacao2 = "filha";
+    String relacao2 = "primo";
     Relation relation2 = new Relation(relacao2);
     boolean admin2 = false;
 
-    // add member1 and member2 to family
+    int family1ID = 1;
 
-    FamilyMember maria = new FamilyMember(cc1, name1, date1, numero1, email1, nif1, rua1, codPostal1, local1, city2, admin1);
+    @BeforeEach
+    void setup() {
+        familyService.addFamily("Silva");
+        familyService.addFamilyAdministrator(cc, name, date, numero, email, nif, rua, codPostal, local, city, family1ID);
+        familyService.addFamilyMember(cc,id2,name2,date2,numero2,email2,nif2,rua2,codPostal2,local2,city2,family1ID);
+    }
 
     @Test
     void addCreditCardAccountToFamilyMemberTrue() {
-
-        Application app = new Application();
-        FamilyMember ze = new FamilyMember(cc1, name1, date1, numero1, email1, nif1, rua1, codPostal1, local1, city2, admin1);
-        AddCreditCardAccountController addCreditCardAccountController = new AddCreditCardAccountController(app);
-        /*CreditCardAccount visaZe = new CreditCardAccount(5000,"Visa do Ze",12 );
-        addCreditCardAccountController.addCreditCardAccountToFamilyMember(ze.getID(), family1ID,"Cartao VISA Ze", 1000);
-*/
-
+        Assertions.assertTrue(addCreditCardAccountController.addCreditCardAccountToFamilyMember(id2, family1ID, "VISA do Diogo", 5000));
     }
 
     @Test
     void addCreditCardAccountToFamilyMemberTrueWithCardDescriptionNull() {
-
+        assertTrue(addCreditCardAccountController.addCreditCardAccountToFamilyMember(id2, 1, null, 5000));
     }
 
     @Test
     void addCreditCardAccountToFamilyMemberFalseFamilyDoesNotExist() {
-
+        assertFalse(addCreditCardAccountController.addCreditCardAccountToFamilyMember(id2, 2, "MasterCard do Diogo", 5000));
     }
 
     @Test
     void addCreditCardAccountToFamilyMemberFalseFamilyMemberDoesNotExist() {
+        assertFalse(addCreditCardAccountController.addCreditCardAccountToFamilyMember("3", family1ID, "MasterCard do Diogo", 5000));
     }
 }
