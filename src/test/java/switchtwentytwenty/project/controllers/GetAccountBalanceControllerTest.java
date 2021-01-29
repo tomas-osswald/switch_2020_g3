4739158c796bd311349d2e9e39df552659f9f51b
Manuel Almeida[1201770] <1201770@isep.ipp.pt>
2021-01-29 13:23:26 +0000

    added tests for all accounts in GetAccountBalanceController

--- src/test/java/switchtwentytwenty/project/controllers/GetAccountBalanceControllerTest.java
+++ src/test/java/switchtwentytwenty/project/controllers/GetAccountBalanceControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import switchtwentytwenty.project.domain.DTOs.MoneyValue;
 import switchtwentytwenty.project.domain.DTOs.input.AddCashAccountDTO;
+import switchtwentytwenty.project.domain.DTOs.input.AddCreditCardAccountDTO;
 import switchtwentytwenty.project.domain.model.Application;
 import switchtwentytwenty.project.domain.model.Family;
 import switchtwentytwenty.project.domain.model.FamilyMember;
@@ -23,6 +24,9 @@ class GetAccountBalanceControllerTest {
     AddFamilyController addFamilyController = new AddFamilyController(ffmApp);
     AddFamilyAdministratorController addFamilyAdministratorController = new AddFamilyAdministratorController(ffmApp);
     AddBankAccountController addBankAccountController = new AddBankAccountController(ffmApp);
+    AddBankSavingsAccountController addBankSavingsAccountController = new AddBankSavingsAccountController(ffmApp);
+    AddCreditCardAccountController addCreditCardAccountController = new AddCreditCardAccountController(ffmApp);
+    CreatePersonalCashAccountController createPersonalCashAccountController = new CreatePersonalCashAccountController(ffmApp);
     GetAccountBalanceController getAccountBalanceController = new GetAccountBalanceController(ffmApp);
 
     String cc = "000000000ZZ4";
@@ -38,21 +42,33 @@ class GetAccountBalanceControllerTest {
     String relacao = "filho";
 
 
-    double balanceAmount = 0.6;
+    double bankAccBalance = 0.6;
+    double savingsAccBalance = 15;
+    double cashAccBalance = 5;
     double negativeBalance = -2;
-    String accountName = "Current";
-    MoneyValue accountBalance = new MoneyValue(balanceAmount, CurrencyEnum.EURO);
+    double interestRate = 5;
+    String currentAccName = "Current";
+    String savingsAccName = "Savings";
+    String cashAccName = "Cash";
+    String creditCardAccName = "Credit Card";
+    MoneyValue accountBalance = new MoneyValue(bankAccBalance, CurrencyEnum.EURO);
 
     @BeforeEach
     void setUp() {
 
         addFamilyController.addFamily("Ribeiro");
         addFamilyAdministratorController.addFamilyAdministrator(cc, name, date, numero, email, nif, rua, codPostal, local, city, 1);
-        addBankAccountController.addBankAccount(accountName, 1, cc, balanceAmount);
+        addBankAccountController.addBankAccount(currentAccName, 1, cc, bankAccBalance);
+        addBankSavingsAccountController.addBankSavingsAccount(1, cc, savingsAccName, savingsAccBalance, interestRate);
+
+        AddCreditCardAccountDTO addCreditCardAccountDTO = new AddCreditCardAccountDTO(cc, 1, creditCardAccName, 5000.00);
+        AddCashAccountDTO addCashAccountDTO = new AddCashAccountDTO(cashAccBalance, cashAccName, cc, 1);
+        addCreditCardAccountController.addCreditCardAccountToFamilyMember(addCreditCardAccountDTO);
+        createPersonalCashAccountController.createPersonalCashAccount(addCashAccountDTO);
     }
 
     @Test
-    void getAccountBalanceTest1_Success() {
+    void getBankAccountBalanceTest1_Success() {
         double expected = 0.6;
 
         double result = getAccountBalanceController.getAccountBalance(1, cc, 1);
@@ -60,12 +76,66 @@ class GetAccountBalanceControllerTest {
         Assertions.assertEquals(expected, result, 0.01);
     }
     @Test
-    void getAccountBalanceTest2_NotEquals() {
+    void getBankAccountBalanceTest2_NotEquals() {
 
         //expected value is purposefully incorrect
         double expected = 0.9;
 
         double result = getAccountBalanceController.getAccountBalance(1, cc, 1);
+
+        Assertions.assertNotEquals(expected, result, 0.01);
+    }
+    @Test
+    void getSavingsAccountBalanceTest1_Success() {
+        double expected = 15;
+
+        double result = getAccountBalanceController.getAccountBalance(1, cc, 2);
+
+        Assertions.assertEquals(expected, result, 0.01);
+    }
+    @Test
+    void getSavingsAccountBalanceTest2_NotEquals() {
+
+        //expected value is purposefully incorrect
+        double expected = 0.9;
+
+        double result = getAccountBalanceController.getAccountBalance(1, cc, 2);
+
+        Assertions.assertNotEquals(expected, result, 0.01);
+    }
+    @Test
+    void getCreditCardAccountBalanceTest1_Success() {
+        double expected = 5000;
+
+        double result = getAccountBalanceController.getAccountBalance(1, cc, 3);
+
+        Assertions.assertEquals(expected, result, 0.01);
+    }
+    @Test
+    void getCreditCardAccountBalanceTest2_NotEquals() {
+
+        //expected value is purposefully incorrect
+        double expected = 0.9;
+
+        double result = getAccountBalanceController.getAccountBalance(1, cc, 3);
+
+        Assertions.assertNotEquals(expected, result, 0.01);
+    }
+    @Test
+    void getCashAccountBalanceTest1_Success() {
+        double expected = 5;
+
+        double result = getAccountBalanceController.getAccountBalance(1, cc, 4);
+
+        Assertions.assertEquals(expected, result, 0.01);
+    }
+    @Test
+    void getCashAccountBalanceTest2_NotEquals() {
+
+        //expected value is purposefully incorrect
+        double expected = 0.9;
+
+        double result = getAccountBalanceController.getAccountBalance(1, cc, 4);
 
         Assertions.assertNotEquals(expected, result, 0.01);
     }
