756f717fe1cacf7adbbaef5b0f501a0757eab41e
João Pinto <1201765@isep.ipp.pt>
2021-02-01 18:14:48 +0000

    mutation hunting

--- src/main/java/switchtwentytwenty/project/domain/dtos/input/AddCreditCardAccountDTO.java
+++ src/main/java/switchtwentytwenty/project/domain/dtos/input/AddCreditCardAccountDTO.java
@@ -4,13 +4,13 @@ import switchtwentytwenty.project.domain.utils.CurrencyEnum;
 
 public class AddCreditCardAccountDTO {
 
-    final private String familyMemberID;
-    final private int familyID;
-    final private String cardDescription;
-    final private Double withdrwaLimit;
-    final private Double totalDebt;
-    final private Double interestDebt;
-    final private CurrencyEnum currency;
+    private final String familyMemberID;
+    private final int familyID;
+    private final String cardDescription;
+    private final Double withdrwaLimit;
+    private final Double totalDebt;
+    private final Double interestDebt;
+    private final CurrencyEnum currency;
 
     public AddCreditCardAccountDTO(String familyMemberID, int familyID, String cardDescription, Double withdrwaLimit, Double totalDebt, Double interestDebt, CurrencyEnum currency) {
         this.familyMemberID = familyMemberID;
---
+++ src/test/java/switchtwentytwenty/project/domain/dtos/input/AddCreditCardAccountDTOTest.java
@@ -0,0 +1,16 @@
+package switchtwentytwenty.project.domain.dtos.input;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddCreditCardAccountDTOTest {
+
+    @Test
+    void getCardDescription() {
+    }
+
+    @Test
+    void getCurrency() {
+    }
+}
