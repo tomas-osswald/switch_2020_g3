845ac00a2c28e53a50e5a0aa9042e50a3c4373a7
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-02-04 15:15:17 +0000

    more javadocs

--- src/main/java/switchtwentytwenty/project/controllers/ChangeRelationBaseController.java
+++ src/main/java/switchtwentytwenty/project/controllers/ChangeRelationBaseController.java
@@ -12,14 +12,14 @@ public class ChangeRelationBaseController {
     }
 
     /**
-     *
-     * @param adminCC
-     * @param memberACCNumber
-     * @param memberBCCNumber
-     * @param familyID
-     * @param relationDesignation
-     * @param aIsParentOfB
-     * @return
+     * Controller level method to edit an existing relation between 2 family members
+     * @param adminCC ID number of the family administrator
+     * @param memberACCNumber ID number of the first member of the relationship
+     * @param memberBCCNumber ID number of the second member of the relationship
+     * @param familyID ID number of the target family
+     * @param relationDesignation Name of the relation
+     * @param aIsParentOfB True if the first member has parental permissions over the second
+     * @return True if the relation is successfully changed
      */
     public boolean changeRelation(String adminCC, String memberACCNumber, String memberBCCNumber, int familyID, String relationDesignation, boolean aIsParentOfB) {
         AddOrChangeRelationController changeRelationController = new AddOrChangeRelationController();
