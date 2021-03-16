edcfc4e14311a8f486f434f6945dd4c58a3aaae1
Diogo Ribeiro [1201768] <1201768@isep.ipp.pt>
2021-03-16 22:30:50 +0000

    fix merge conflits

--- src/main/java/switchtwentytwenty/project/category/Category.java
+++ src/main/java/switchtwentytwenty/project/category/Category.java
@@ -4,15 +4,19 @@ import switchtwentytwenty.project.AggregateRoot;
 import switchtwentytwenty.project.shared.CategoryName;
 import switchtwentytwenty.project.shared.CategoryID;
 
-public class Category implements AggregateRoot {
+public class Category implements AggregateRoot{
 
-    private CategoryID categoryID;
     private CategoryName categoryName;
+    private CategoryID categoryID;
+    private int postion;
 
-    public Category(CategoryID categoryID, CategoryName categoryName) {
-        this.categoryID = categoryID;
+    public Category(CategoryName categoryName, CategoryID categoryID) {
         this.categoryName = categoryName;
+        this.categoryID = categoryID;
     }
 
-    
+
+
+
+
 }
