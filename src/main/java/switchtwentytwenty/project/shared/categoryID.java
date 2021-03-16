64729483c28ec25be0759fcdcd662b56a9853e34
Diogo Ribeiro [1201768] <1201768@isep.ipp.pt>
2021-03-16 20:08:24 +0000

    add category aggregate and value objects

---
+++ src/main/java/switchtwentytwenty/project/shared/categoryID.java
@@ -0,0 +1,15 @@
+package switchtwentytwenty.project.shared;
+
+import switchtwentytwenty.project.ID;
+import switchtwentytwenty.project.ValueObject;
+
+public class categoryID implements ValueObject, ID {
+    private int categoryID;
+
+    public categoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    
+
+}
---
+++ src/main/java/switchtwentytwenty/project/shared/categoryName.java
@@ -0,0 +1,37 @@
+package switchtwentytwenty.project.shared;
+
+import switchtwentytwenty.project.ValueObject;
+import switchtwentytwenty.project.exceptions.InvalidNameException;
+
+public class categoryName implements ValueObject {
+    private String name;
+
+    public categoryName(String name) {
+        this.name = name;
+        validation();
+    }
+
+    private void validation(){
+        checkIfNotNull();
+        checkIfNotBlank();
+        checkIfNotEmpty();
+    }
+
+    private void checkIfNotNull(){
+        if (this.name == null){
+            throw new InvalidNameException("Name cannot be null");
+        }
+    }
+
+    private void checkIfNotBlank(){
+        if (this.name == ""){
+            throw new InvalidNameException("Name cannot be blank");
+        }
+    }
+
+    private void checkIfNotEmpty(){
+        if (this.name == ""){
+            throw new InvalidNameException("Name cannot be empty");
+        }
+    }
+}
