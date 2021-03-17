3e68819814ac014cab1be2555cc3c85cc565635d
Diogo Ribeiro [1201768] <1201768@isep.ipp.pt>
2021-03-17 10:10:57 +0000

    fix package issues

--- src/main/java/switchtwentytwenty/project/category/Category.java
+++ src/main/java/switchtwentytwenty/project/category/Category.java
@@ -1,20 +1,18 @@
 package switchtwentytwenty.project.category;
 
 import switchtwentytwenty.project.AggregateRoot;
-import switchtwentytwenty.project.shared.CategoryName;
 import switchtwentytwenty.project.shared.CategoryID;
+import switchtwentytwenty.project.shared.CategoryName;
 
-public class Category implements AggregateRoot{
+public class Category implements AggregateRoot {
 
     private CategoryName categoryName;
     private CategoryID categoryID;
-    private int postion;
 
     public Category(CategoryName categoryName, CategoryID categoryID) {
         this.categoryName = categoryName;
         this.categoryID = categoryID;
     }
 
-
-
+    
 }
