20a3806d8343b74b57b5885a86f8ae775a00061a
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-03-17 10:46:50 +0000

    tamal

--- src/main/java/switchtwentytwenty/project/category/Category.java
+++ src/main/java/switchtwentytwenty/project/category/Category.java
@@ -1,14 +1,15 @@
 package switchtwentytwenty.project.Category;
 
 import switchtwentytwenty.project.AggregateRoot;
-import switchtwentytwenty.project.shared.CategoryID;
-import switchtwentytwenty.project.shared.CategoryName;
+import switchtwentytwenty.project.Category.shared.CategoryID;
+import switchtwentytwenty.project.Category.shared.CategoryName;
 
 public class Category implements AggregateRoot{
 
     private CategoryName categoryName;
     private CategoryID categoryID;
     private int position;
+
 
     public Category(CategoryName categoryName, CategoryID categoryID) {
         this.categoryName = categoryName;
