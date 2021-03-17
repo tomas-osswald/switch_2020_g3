bb4cecbb1a0583b9272f54af4bc0d8b087dca7ee
Diogo Ribeiro [1201768] <1201768@isep.ipp.pt>
2021-03-17 09:53:31 +0000

    add categoryName tests

--- src/main/java/switchtwentytwenty/project/category/Category.java
+++ src/main/java/switchtwentytwenty/project/category/Category.java
@@ -17,6 +17,4 @@ public class Category implements AggregateRoot{
 
 
 
-
-
 }
--- src/test/java/switchtwentytwenty/project/shared/categoryNameTest.java
+++ src/test/java/switchtwentytwenty/project/shared/categoryNameTest.java
@@ -1,28 +1,43 @@
 package switchtwentytwenty.project.shared;
 
 import org.junit.jupiter.api.Test;
+import switchtwentytwenty.project.category.Category;
+import switchtwentytwenty.project.exceptions.InvalidNameException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class categoryNameTest {
 
+    /*
     @Test
     void categoryNameConstructor_Valid(){
-
+        String name = "TonyZe";
+        CategoryName categoryName;
+        assertDoesNotThrow( ()-> categoryName = new CategoryName(name));
     }
+     */
 
     @Test
     void categoryNameConstructor_Invalid_Null(){
-
+        String name = null;
+        assertThrows(InvalidNameException.class,()-> {
+            CategoryName categoryName = new CategoryName(name);
+        });
     }
 
     @Test
     void categoryNameConstructor_Invalid_Empty(){
-
+        String name = "";
+        assertThrows(InvalidNameException.class,()-> {
+            CategoryName categoryName = new CategoryName(name);
+        });
     }
 
     @Test
     void categoryNameConstructor_Invalid_Blank(){
-
+        String name = "      ";
+        assertThrows(InvalidNameException.class,()-> {
+            CategoryName categoryName = new CategoryName(name);
+        });
     }
 }
