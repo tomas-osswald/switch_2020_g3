0c43d1a041f78bd9b989c8a9203a0d254b05c06f
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-03-24 16:28:56 +0000

    completed CreateFamilyDTO.java

--- src/main/java/switchtwentytwenty/project/dto/CreateFamilyDTO.java
+++ src/main/java/switchtwentytwenty/project/dto/CreateFamilyDTO.java
@@ -2,17 +2,74 @@ package switchtwentytwenty.project.dto;
 
 public class CreateFamilyDTO {
 
-    
 
-    public String unpackAdminEmail;
-    public String unpackFamilyName;
-    public String unpackName;
-    public String unpackBirthDate;
-    public int unpackVAT;
-    public int unpackPhone;
-    public String unpackStreet;
-    public String unpackCity;
-    public int unpackHouseNumber;
-    public String unpackZipCode;
-    public String unpackCCNumber;
+    private String adminEmail;
+    private String familyName;
+    private String name;
+    private String birtDate;
+    private int vatNumber;
+    private int phone;
+    private String street;
+    private String city;
+    private int houseNumber;
+    private String zipCode;
+    private String ccNumber;
+
+    public CreateFamilyDTO(String adminEmail, String familyName, String name, String birtDate, int vatNumber, int phone, String street, String city, int houseNumber, String zipCode, String ccNumber) {
+        this.adminEmail = adminEmail;
+        this.familyName = familyName;
+        this.name = name;
+        this.birtDate = birtDate;
+        this.vatNumber = vatNumber;
+        this.phone = phone;
+        this.street = street;
+        this.city = city;
+        this.houseNumber = houseNumber;
+        this.zipCode = zipCode;
+        this.ccNumber = ccNumber;
+    }
+
+    public String unpackAdminEmail() {
+        return this.adminEmail;
+    }
+
+    public String unpackFamilyName() {
+        return this.familyName;
+    }
+
+    public String unpackName() {
+        return this.name;
+    }
+
+    public String unpackBirthDate() {
+        return this.birtDate;
+    }
+
+    public int unpackVAT() {
+        return this.vatNumber;
+    }
+
+    public int unpackPhone() {
+        return this.phone;
+    }
+
+    public String unpackStreet() {
+        return this.street;
+    }
+
+    public String unpackCity() {
+        return this.city;
+    }
+
+    public int unpackHouseNumber() {
+        return this.houseNumber;
+    }
+
+    public String unpackZipCode() {
+        return this.zipCode;
+    }
+
+    public String unpackCCNumber() {
+        return this.ccNumber;
+    }
 }
