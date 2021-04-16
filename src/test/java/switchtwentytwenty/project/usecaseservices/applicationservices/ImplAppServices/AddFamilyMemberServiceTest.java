5a2d0d5d6c7864bd57f5f43f8e5db00e0da7d5c5
Tomás Osswald [1201784] <1201784@isep.ipp.pt>
2021-04-16 13:10:53 +0100

    Fix to test

--- src/test/java/switchtwentytwenty/project/usecaseservices/applicationservices/ImplAppServices/AddFamilyMemberServiceTest.java
+++ src/test/java/switchtwentytwenty/project/usecaseservices/applicationservices/ImplAppServices/AddFamilyMemberServiceTest.java
@@ -30,34 +30,19 @@ class AddFamilyMemberServiceTest {
     @Mock
     AddPersonDTO addPersonDTO;
 
-    @Mock
-    PersonID personID;
+    //@Mock
+    //PersonID personID;
 
     @Mock
-    PersonID loggedUserID;
+    Person admin;
 
-    final String VALIDNAME = "TonyZe";
-    final String VALIDEMAIL = "tonyze@latinlover.pt";
-    final int VALIDVATNUMBER = 999999999;
-    final Integer VALIDPHONENUMBER = 916969696;
-    final String VALIDSTREET = "Rua";
-    final String VALIDCITY = "Ermesinde";
-    final String VALIDZIPCODE = "4700-111";
-    final int VALIDADDRESSNUMBER = 69;
-    final String VALIDBIRTHDATE = "01/03/1990";
+    //@Mock
+    //FamilyID familyID;
 
-    Name tonyZeName = new Name(VALIDNAME);
-    BirthDate tonyZeBirthDate = new BirthDate(VALIDBIRTHDATE);
-    PersonID tonyZeEmail = new PersonID(VALIDEMAIL);
-    VATNumber tonyZeVat = new VATNumber(VALIDVATNUMBER);
-    PhoneNumber tonyZePhone = new PhoneNumber(VALIDPHONENUMBER);
-    Address tonyZeAddress = new Address(VALIDSTREET, VALIDCITY, VALIDZIPCODE, VALIDADDRESSNUMBER);
-    FamilyID familyID = new FamilyID(UUID.randomUUID());
+    //PersonID loggedUserIDtest = new PersonID("tonyze@latinas.com");
 
-    @Mock
-    Person admin = new Person(tonyZeName, tonyZeBirthDate, tonyZeEmail, tonyZeVat, tonyZePhone, tonyZeAddress, familyID);
-    /*@Mock
-    FamilyID familyID;*/
+    //@Mock
+    //PersonID loggedUserID;
 
     @InjectMocks
     AddFamilyMemberService addFamilyMemberService;
@@ -66,20 +51,25 @@ class AddFamilyMemberServiceTest {
     void addPersonSuccess() {
 
         Mockito.when(addPersonDTO.unpackUserID()).thenReturn("tonyze@latinas.com");
-        Mockito.doNothing().when(familyRepository).verifyAdmin(loggedUserID);
+     //   Mockito.doNothing().when(familyRepository).verifyAdmin(loggedUserID);
         Mockito.when(addPersonDTO.unpackName()).thenReturn("tonyze@latinas.com");
         Mockito.when(addPersonDTO.unpackBirthDate()).thenReturn("10/10/1999");
         Mockito.when(addPersonDTO.unpackEmail()).thenReturn("tonyze@latinas.com");
         Mockito.when(addPersonDTO.unpackVAT()).thenReturn(123456789);
         Mockito.when(addPersonDTO.unpackPhone()).thenReturn(961962963);
-        Mockito.when(addPersonDTO.unpackStreet()).thenReturn("Rua das Irmas Beleza e do Primo Flavio");
+        Mockito.when(addPersonDTO.unpackStreet()).thenReturn("Rua das Irma's Beleza e do Primo Flavio");
         Mockito.when(addPersonDTO.unpackCity()).thenReturn("Gaya");
         Mockito.when(addPersonDTO.unpackZipCode()).thenReturn("1000");
         Mockito.when(addPersonDTO.unpackHouseNumber()).thenReturn(666);
 
+        PersonID loggedUserID = new PersonID("tonyze@latinas.com");
+        PersonID personID = new PersonID("tonyze@latinas.com");
+        FamilyID familyID = new FamilyID(UUID.randomUUID());
+
         Mockito.when(personRepository.isPersonIDAlreadyRegistered(personID)).thenReturn(false);
-        Mockito.doReturn(admin).when(personRepository).getByID(loggedUserID);
-//        Mockito.when(admin.getFamilyID()).thenReturn(familyID);
+        Mockito.when(personRepository.getByID(loggedUserID)).thenReturn(admin);
+        Mockito.when(admin.getFamilyID()).thenReturn(familyID);
+
         assertDoesNotThrow(() -> addFamilyMemberService.addPerson(addPersonDTO));
     }
 }
