7b21f79dbd7c64710868f5d5df08604ffff24079
Miguel Loureiro [11819455] <1181945@isep.ipp.pt>
2021-04-21 11:33:06 +0100

    add EmailAddressJPA

---
+++ src/main/java/switchtwentytwenty/project/datamodel/EmailAddressJPA.java
@@ -0,0 +1,35 @@
+package switchtwentytwenty.project.datamodel;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+
+@Entity
+@Table(name = "emailAddresses")
+public class EmailAddressJPA {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String email;
+
+
+    @ManyToOne()
+    @JoinColumn(name = "personID", nullable=false)
+    //because it is in the same aggregate, a foreign key constraint is used
+    private PersonJPA person;
+
+    public EmailAddressJPA(String email) {
+        this.email =email;
+
+    }
+}
