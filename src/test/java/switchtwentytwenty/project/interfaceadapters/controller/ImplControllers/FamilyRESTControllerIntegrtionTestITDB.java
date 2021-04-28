6d43d1719d744e3cbff1c8dea02ec0c97dfc078a
João Pinto [1201765] <1201765@isep.ipp.pt>
2021-04-28 20:40:27 +0100

    changes in pom.xml, pitest will ignore *ITDB test classes (integration tests with database)

---
+++ src/test/java/switchtwentytwenty/project/interfaceadapters/controller/ImplControllers/FamilyRESTControllerIntegrtionTestITDB.java
@@ -0,0 +1,46 @@
+package switchtwentytwenty.project.interfaceadapters.controller.ImplControllers;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.hateoas.Link;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import switchtwentytwenty.project.dto.AddFamilyAndSetAdminDTO;
+import switchtwentytwenty.project.dto.FamilyOutputDTO;
+import switchtwentytwenty.project.interfaceadapters.controller.IControllers.IFamilyRESTController;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class FamilyRESTControllerIntegrtionTestITDB {
+    AddFamilyAndSetAdminDTO dto = new AddFamilyAndSetAdminDTO("tony@email.com","Silva","12/12/1222",999999999,919999999,"Rua","Cidade","12B","4400-123","Silva","12/12/2000");
+
+
+    @Autowired
+    IFamilyRESTController familyRESTController;
+
+    @Disabled
+    @Test
+    void createFamilyAndSetAdmin() {
+
+        Link expectedLink = linkTo(methodOn(FamilyRESTController.class).getFamilyName(dto.getFamilyName())).withSelfRel();
+
+        FamilyOutputDTO familyOutputDTO = new FamilyOutputDTO("Silva", "tony@email.com", "tony@email.com");
+
+        familyOutputDTO.add(expectedLink);
+
+        ResponseEntity expected = new ResponseEntity(familyOutputDTO, HttpStatus.CREATED);
+
+        ResponseEntity result = familyRESTController.createFamilyAndSetAdmin(dto);
+
+        assertEquals(expected,result);
+    }
+
+}
