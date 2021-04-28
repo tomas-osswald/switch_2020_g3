e303a7a65cecc58d1b86d291a78a339c57ca4ef8
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-04-28 14:06:03 +0100

    Corrected categoryRepo class

--- src/main/java/switchtwentytwenty/project/interfaceadapters/controller/ImplControllers/FamilyRESTController.java
+++ src/main/java/switchtwentytwenty/project/interfaceadapters/controller/ImplControllers/FamilyRESTController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sun.plugin.javascript.navig.Link;
 import switchtwentytwenty.project.dto.AddFamilyAndSetAdminDTO;
 import switchtwentytwenty.project.dto.InputFamilyDTO;
 import switchtwentytwenty.project.dto.InputPersonDTO;
