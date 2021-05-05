f3c38eaa4aa3cd8bceeb710d720692337d8aa22e
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-05-05 15:26:55 +0100

    Created tests for InputFamilyDTO

---
+++ src/test/java/switchtwentytwenty/project/dto/family/InputFamilyDTOTest.java
@@ -0,0 +1,50 @@
+package switchtwentytwenty.project.dto.family;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputFamilyDTOTest {
+
+    @Test
+    @DisplayName("Should return true if two identical InputFamilyDTO objects are compared using the equals method")
+    void testEquals() {
+        InputFamilyDTO inputFamilyDTO1 = new InputFamilyDTO("Silva", "12/12/1990");
+        InputFamilyDTO inputFamilyDTO2 = new InputFamilyDTO("Silva", "12/12/1990");
+
+        assertEquals(inputFamilyDTO1, inputFamilyDTO2);
+        assertNotSame(inputFamilyDTO1, inputFamilyDTO2);
+    }
+
+    @Test
+    @DisplayName("Should return false if two different InputFamilyDTO objects are compared using the equals method")
+    void testEqualsFail() {
+        InputFamilyDTO inputFamilyDTO1 = new InputFamilyDTO("Silvas", "12/12/1990");
+        InputFamilyDTO inputFamilyDTO2 = new InputFamilyDTO("Silva", "12/12/1990");
+
+        assertNotEquals(inputFamilyDTO1, inputFamilyDTO2);
+        assertNotSame(inputFamilyDTO1, inputFamilyDTO2);
+    }
+
+
+    @Test
+    @DisplayName("Should return true if two identical InputFamilyDTO objects are compared using their hash codes")
+    void testHashCode() {
+        InputFamilyDTO inputFamilyDTO1 = new InputFamilyDTO("Silva", "12/12/1990");
+        InputFamilyDTO inputFamilyDTO2 = new InputFamilyDTO("Silva", "12/12/1990");
+
+        assertEquals(inputFamilyDTO1.hashCode(), inputFamilyDTO2.hashCode());
+        assertNotSame(inputFamilyDTO1, inputFamilyDTO2);
+    }
+
+    @Test
+    @DisplayName("Should return false if two different InputFamilyDTO objects are compared using their hash codes")
+    void testHashCodeFail() {
+        InputFamilyDTO inputFamilyDTO1 = new InputFamilyDTO("Silva", "12/12/1990");
+        InputFamilyDTO inputFamilyDTO2 = new InputFamilyDTO("Silvas", "12/12/1990");
+
+        assertNotEquals(inputFamilyDTO1.hashCode(), inputFamilyDTO2.hashCode());
+        assertNotSame(inputFamilyDTO1, inputFamilyDTO2);
+    }
+}
