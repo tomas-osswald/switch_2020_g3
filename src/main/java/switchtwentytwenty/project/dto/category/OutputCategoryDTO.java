d35bb8f6bb198c647d822912fb70f638566f1f86
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-05-05 15:20:40 +0100

    Created tests for AddFamilyAndSetAdminDTO

--- src/main/java/switchtwentytwenty/project/dto/category/OutputCategoryDTO.java
+++ src/main/java/switchtwentytwenty/project/dto/category/OutputCategoryDTO.java
@@ -7,7 +7,6 @@ public class OutputCategoryDTO {
     private String categoryName;
 
     private String categoryID;
-
     private String parentID;
 
     public OutputCategoryDTO(String categoryName, String categoryID, String parentID) {
@@ -16,16 +15,30 @@ public class OutputCategoryDTO {
         this.parentID = parentID;
     }
 
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public String getCategoryID() {
+        return categoryID;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         OutputCategoryDTO that = (OutputCategoryDTO) o;
-        return categoryID.equals(that.categoryID);
+        return Objects.equals(categoryName, that.categoryName) && Objects.equals(categoryID, that.categoryID) && Objects.equals(parentID, that.parentID);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(categoryID);
+        return Objects.hash(categoryName, categoryID, parentID);
     }
+
+    public String getParentID() {
+        return parentID;
+    }
+
+
 }
