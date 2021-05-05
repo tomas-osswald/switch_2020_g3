e1881225601025b9ab4f3a4ec54f6e24ad7d5f4a
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-05-05 09:34:26 +0100

    added tags to AccountJPA and AccountIDJPA

--- src/main/java/switchtwentytwenty/project/datamodel/domainjpa/AccountIDJPA.java
+++ src/main/java/switchtwentytwenty/project/datamodel/domainjpa/AccountIDJPA.java
@@ -1,4 +1,12 @@
 package switchtwentytwenty.project.datamodel.domainjpa;
 
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Embeddable;
+
+@ToString
+@NoArgsConstructor
+@Embeddable
 public class AccountIDJPA {
 }
--- src/main/java/switchtwentytwenty/project/datamodel/domainjpa/AccountJPA.java
+++ src/main/java/switchtwentytwenty/project/datamodel/domainjpa/AccountJPA.java
@@ -1,4 +1,24 @@
 package switchtwentytwenty.project.datamodel.domainjpa;
 
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@NoArgsConstructor
+@Entity
+@Table(name = "accounts")
 public class AccountJPA {
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
 }
