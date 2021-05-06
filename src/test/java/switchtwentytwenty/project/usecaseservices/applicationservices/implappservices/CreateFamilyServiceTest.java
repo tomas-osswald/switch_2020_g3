d35bedd10098c7fd80906d18e4a08a0fd05a87d9
Bruno Tamames [1201762] <1201762@isep.ipp.pt>
2021-05-06 10:07:01 +0100

    Fix to FamilyService tests

--- src/test/java/switchtwentytwenty/project/usecaseservices/applicationservices/implappservices/CreateFamilyServiceTest.java
+++ src/test/java/switchtwentytwenty/project/usecaseservices/applicationservices/implappservices/CreateFamilyServiceTest.java
@@ -13,8 +13,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import switchtwentytwenty.project.domain.aggregates.family.Family;
 import switchtwentytwenty.project.domain.aggregates.person.Person;
 import switchtwentytwenty.project.domain.valueobject.FamilyID;
+import switchtwentytwenty.project.domain.valueobject.PersonID;
 import switchtwentytwenty.project.dto.assemblers.implassemblers.FamilyDTODomainAssembler;
 import switchtwentytwenty.project.dto.family.InputFamilyDTO;
+import switchtwentytwenty.project.dto.family.OutputFamilyDTO;
 import switchtwentytwenty.project.dto.person.InputPersonDTO;
 import switchtwentytwenty.project.dto.assemblers.implassemblers.PersonDTODomainAssembler;
 import switchtwentytwenty.project.exceptions.InvalidNameException;
@@ -56,36 +58,41 @@ class CreateFamilyServiceTest {
     @InjectMocks
     CreateFamilyService createFamilyService;
 
+    String familyName = "Silva";
+    String familyID = "@admin@gmail.com";
+    String adminID = "admin@gmail.com";
+    String registrationDate = "12/5/1999";
+
     @BeforeEach
     void setup(){
         Mockito.when(inputPersonDTO.unpackEmail()).thenReturn("admin@email.com");
     }
 
-    //TODO: Decidir o familyID
     @Test
     @Tag("US010")
     @DisplayName("createFamilyAndAdmin Test - Valid data doesn't throw exception")
     void createFamilyAndAddAdminTestValidData(){
-        //FamilyID familyID = new FamilyID("tonyze@latinas.com");
-        Mockito.when(personDTODomainAssembler.toDomain(any(),any())).thenReturn(admin);
-        Mockito.when(familyDTODomainAssembler.toDomain(any(),any(),any())).thenReturn(family);
+        Mockito.when(personDTODomainAssembler.toDomain(any(InputPersonDTO.class),any(FamilyID.class))).thenReturn(admin);
+        Mockito.when(familyDTODomainAssembler.toDomain(any(InputFamilyDTO.class),any(FamilyID.class),any(PersonID.class))).thenReturn(family);
 
         Mockito.when(personRepository.add(admin)).thenReturn(admin);
         Mockito.when(familyRepository.add(family)).thenReturn(family);
 
-        assertDoesNotThrow(() -> createFamilyService.createFamilyAndAddAdmin(inputFamilyDTO, inputPersonDTO));
+        Mockito.when(familyDTODomainAssembler.toDTO(any(Family.class))).thenReturn(new OutputFamilyDTO(familyName,familyID,adminID,registrationDate));
+
+        OutputFamilyDTO outputFamilyDTO = createFamilyService.createFamilyAndAddAdmin(inputFamilyDTO, inputPersonDTO);
+        assertNotNull(outputFamilyDTO);
     }
 
     @Test
     @Tag("US010")
     @DisplayName("createFamilyAndAdmin Test - Invalid person name, should throw exception")
     void createFamilyAndAddAdminTestInvalidName(){
-        //FamilyID familyID = new FamilyID("tonyze@latinas.com");
-        //Mockito.when(familyRepository.generateID()).thenReturn(familyID);
-        Mockito.when(personDTODomainAssembler.toDomain(any(),any())).thenThrow(InvalidNameException.class);
-        Mockito.when(familyDTODomainAssembler.toDomain(any(),any(),any())).thenReturn(family);
+        Mockito.when(personDTODomainAssembler.toDomain(any(InputPersonDTO.class),any(FamilyID.class))).thenThrow(InvalidNameException.class);
+        Mockito.when(familyDTODomainAssembler.toDomain(any(InputFamilyDTO.class),any(FamilyID.class),any(PersonID.class))).thenReturn(family);
         Mockito.when(personRepository.add(admin)).thenReturn(admin);
         Mockito.when(familyRepository.add(family)).thenReturn(family);
+        Mockito.when(familyDTODomainAssembler.toDTO(any(Family.class))).thenReturn(new OutputFamilyDTO(familyName,familyID,adminID,registrationDate));
 
         assertThrows(InvalidNameException.class,() -> createFamilyService.createFamilyAndAddAdmin(inputFamilyDTO, inputPersonDTO));
     }
@@ -94,12 +101,11 @@ class CreateFamilyServiceTest {
     @Tag("US010")
     @DisplayName("createFamilyAndAdmin Test - Person already registered throw exception")
     void createFamilyAndAddAdminTestPersonAlreadyRegistered(){
-        //FamilyID familyID = new FamilyID("tonyze@latinas.com");
-        //Mockito.when(familyRepository.generateID()).thenReturn(familyID);
-        Mockito.when(personDTODomainAssembler.toDomain(any(),any())).thenReturn(admin);
-        Mockito.when(familyDTODomainAssembler.toDomain(any(),any(),any())).thenReturn(family);
+        Mockito.when(personDTODomainAssembler.toDomain(any(InputPersonDTO.class),any(FamilyID.class))).thenReturn(admin);
+        Mockito.when(familyDTODomainAssembler.toDomain(any(InputFamilyDTO.class),any(FamilyID.class),any(PersonID.class))).thenReturn(family);
         Mockito.doThrow(PersonAlreadyRegisteredException.class).when(personRepository).add(any());
         Mockito.when(familyRepository.add(family)).thenReturn(family);
+        Mockito.when(familyDTODomainAssembler.toDTO(any(Family.class))).thenReturn(new OutputFamilyDTO(familyName,familyID,adminID,registrationDate));
 
         assertThrows(PersonAlreadyRegisteredException.class,() -> createFamilyService.createFamilyAndAddAdmin(inputFamilyDTO, inputPersonDTO));
     }
