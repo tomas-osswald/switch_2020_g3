a2191de944d43f1c21040a2e3822abe30bbc8af1
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-05-07 09:50:24 +0100

    Removed unused, deprecated methods

--- src/main/java/switchtwentytwenty/project/usecaseservices/irepositories/IPersonRepository.java
+++ src/main/java/switchtwentytwenty/project/usecaseservices/irepositories/IPersonRepository.java
@@ -1,15 +1,13 @@
 package switchtwentytwenty.project.usecaseservices.irepositories;
 
 import switchtwentytwenty.project.domain.aggregates.person.Person;
-import switchtwentytwenty.project.domain.valueobject.*;
+import switchtwentytwenty.project.domain.valueobject.PersonID;
 
-public interface IPersonRepository extends Repository <Person, PersonID>{
+public interface IPersonRepository extends Repository<Person, PersonID> {
 
-    FamilyID getPersonFamilyID(PersonID personID);
 
     Person add(Person entity);
 
-    void createAndAdd(Name name, BirthDate birthDate, PersonID personID, VATNumber vat, PhoneNumber phone, Address address, FamilyID familyID);
 
     boolean isPersonIDAlreadyRegistered(PersonID personID);
 
