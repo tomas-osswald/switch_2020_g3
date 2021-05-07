8cd1a58181c1407c1eddee11ae369aa5e51d5fd4
Miguel Loureiro [11819455] <1181945@isep.ipp.pt>
2021-05-07 11:46:48 +0100

    fixing errors

--- src/test/java/switchtwentytwenty/project/interfaceadapters/controller/implcontrollers/GetFamilyMemberProfileControllerTest.java
+++ src/test/java/switchtwentytwenty/project/interfaceadapters/controller/implcontrollers/GetFamilyMemberProfileControllerTest.java
@@ -1,4 +1,4 @@
-package switchtwentytwenty.project.interfaceadapters.controller.ImplControllers;
+package switchtwentytwenty.project.interfaceadapters.controller.implcontrollers;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import switchtwentytwenty.project.dto.InternalProfileDTO;
 import switchtwentytwenty.project.dto.person.OutputPersonDTO;
+import switchtwentytwenty.project.interfaceadapters.controller.implcontrollers.GetFamilyMemberProfileController;
 import switchtwentytwenty.project.usecaseservices.applicationservices.iappservices.IGetFamilyMemberProfileService;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @RunWith(SpringRunner.class)
 class GetFamilyMemberProfileControllerTest {
 
-    OutputPersonDTO outputPersonDTOoutput = new OutputPersonDTO();
+    OutputPersonDTO outputPersonDTO = new OutputPersonDTO();
 
     @Mock
     IGetFamilyMemberProfileService getProfileService;
@@ -35,7 +36,7 @@ class GetFamilyMemberProfileControllerTest {
         String personID = "email@domain.pt";
         OutputPersonDTO expected = new OutputPersonDTO();
 
-        Mockito.when(getProfileService.getFamilyMemberProfile(internalProfileDTO)).thenReturn(outputPersonDTOoutput);
+        Mockito.when(getProfileService.getFamilyMemberProfile(internalProfileDTO)).thenReturn(outputPersonDTO);
 
         OutputPersonDTO result = getFamilyMemberProfileController.getFamilyMemberProfile(internalProfileDTO);
 
