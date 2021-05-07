4db9cc5e1ef03c1e8da364068f1c0cb2da29e5e3
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-05-07 16:16:48 +0100

    created get PersonOptionsDTO and getPersonOptions OPTIONS method

--- src/main/java/switchtwentytwenty/project/interfaceadapters/controller/implcontrollers/PersonRESTController.java
+++ src/main/java/switchtwentytwenty/project/interfaceadapters/controller/implcontrollers/PersonRESTController.java
@@ -1,22 +1,27 @@
 package switchtwentytwenty.project.interfaceadapters.controller.implcontrollers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import switchtwentytwenty.project.dto.AddFamilyMemberDTO;
 import switchtwentytwenty.project.dto.GetProfileInfoDTO;
 import switchtwentytwenty.project.dto.assemblers.implassemblers.FamilyMemberExternalInternalAssembler;
 import switchtwentytwenty.project.dto.family.InternalFamilyMemberDTO;
 import switchtwentytwenty.project.dto.person.AddEmailDTO;
 import switchtwentytwenty.project.dto.person.OutputPersonDTO;
+import switchtwentytwenty.project.dto.person.PersonOptionsDTO;
 import switchtwentytwenty.project.interfaceadapters.controller.icontrollers.IPersonRESTController;
 import switchtwentytwenty.project.usecaseservices.applicationservices.iappservices.IAddFamilyMemberService;
 import switchtwentytwenty.project.usecaseservices.applicationservices.iappservices.IGetFamilyMemberProfileService;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/people")
+@CrossOrigin
 public class PersonRESTController implements IPersonRESTController {
 
     private IGetFamilyMemberProfileService getFamilyMemberProfileService;
@@ -24,6 +29,7 @@ public class PersonRESTController implements IPersonRESTController {
     private FamilyMemberExternalInternalAssembler familyMemberExternalInternalAssembler;
 
     private IAddFamilyMemberService addFamilyMemberService;
+
 
     @Autowired
     public PersonRESTController(IGetFamilyMemberProfileService getFamilyMemberProfileService, IAddFamilyMemberService addFamilyMemberService) {
@@ -41,12 +47,32 @@ public class PersonRESTController implements IPersonRESTController {
         return null;
     }
 
-    public ResponseEntity<Object> addFamilyMember(AddFamilyMemberDTO addFamilyMemberDTO) {
+    @Override
+    @PostMapping("/add")
+    public ResponseEntity<OutputPersonDTO> addFamilyMember(AddFamilyMemberDTO addFamilyMemberDTO) {
         InternalFamilyMemberDTO internalFamilyMemberDTO = familyMemberExternalInternalAssembler.toInner(addFamilyMemberDTO);
-        OutputPersonDTO outputPersonDTO = addFamilyMemberService.addPerson(internalFamilyMemberDTO);
+
+        HttpStatus status;
+        OutputPersonDTO outputPersonDTO;
+
+        try {
+            outputPersonDTO = addFamilyMemberService.addPerson(internalFamilyMemberDTO);
+            status = HttpStatus.CREATED;
+
+            Link personOptionsLink = linkTo(methodOn(PersonRESTController.class).getPersonOptions(outputPersonDTO.getId())).withSelfRel();
+        }catch (Exception e){
+            status = HttpStatus.UNPROCESSABLE_ENTITY;
+        }
         return null;
     }
 
+    @RequestMapping(value = "/{personID}", method = RequestMethod.OPTIONS)
+    public ResponseEntity<PersonOptionsDTO> getPersonOptions(@PathVariable String personID) {
+        PersonOptionsDTO personOptionsDTO = new PersonOptionsDTO();
+        Link getProfileInfo = linkTo(methodOn(PersonRESTController.class).getProfileInfo(new GetProfileInfoDTO(personID))).withRel("Get Profile Info").withType("GET");
+        personOptionsDTO.add(getProfileInfo);
+        return new ResponseEntity<PersonOptionsDTO>(personOptionsDTO, HttpStatus.OK);
+    }
 
     //TODO ver se DTO vem vazio se body do pedido for vazio
     @GetMapping(value = "/{personID}")
