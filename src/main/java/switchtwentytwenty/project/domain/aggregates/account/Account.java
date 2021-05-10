c0c73ba95f18b1bb429b73c520cbbea20fc03907
Miguel Loureiro [1181945] <1181945@isep.ipp.pt>
2021-05-10 20:21:01 +0100

    initialized Account Service

--- src/main/java/switchtwentytwenty/project/domain/aggregates/account/Account.java
+++ src/main/java/switchtwentytwenty/project/domain/aggregates/account/Account.java
@@ -1,8 +1,18 @@
 package switchtwentytwenty.project.domain.aggregates.account;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 import switchtwentytwenty.project.domain.aggregates.AggregateRoot;
 import switchtwentytwenty.project.domain.valueobject.AccountID;
 
+@Component
+@NoArgsConstructor
+@AllArgsConstructor
 public interface Account extends AggregateRoot<AccountID> {
+
+    private AccountID accountID;
+
+
 
 }
---
+++ src/test/java/switchtwentytwenty/project/usecaseservices/applicationservices/implappservices/CreateAccountServiceTest.java
@@ -0,0 +1,14 @@
+package switchtwentytwenty.project.usecaseservices.applicationservices.implappservices;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class CreateAccountServiceTest {
+
+
+}
