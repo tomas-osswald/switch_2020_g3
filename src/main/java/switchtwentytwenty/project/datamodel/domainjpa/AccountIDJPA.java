ab00d7f8f467b12aeab61de8f5a5f4f9eedc1199
Manuel Almeida [1201770] <1201770@isep.ipp.pt>
2021-05-10 23:08:59 +0100

    added AccountIDJPA, AccountJPA and toDomain in AccountDataDomainAssembler

--- src/main/java/switchtwentytwenty/project/datamodel/domainjpa/AccountIDJPA.java
+++ src/main/java/switchtwentytwenty/project/datamodel/domainjpa/AccountIDJPA.java
@@ -4,9 +4,36 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.Objects;
 
 @ToString
 @NoArgsConstructor
 @Embeddable
-public class AccountIDJPA {
+public class AccountIDJPA implements Serializable {
+
+    private Long id;
+
+    public AccountIDJPA (Long id) {
+        this.id = id;
+    }
+
+    public Long toLong () {
+        return this.id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountIDJPA that = (AccountIDJPA) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
 }
