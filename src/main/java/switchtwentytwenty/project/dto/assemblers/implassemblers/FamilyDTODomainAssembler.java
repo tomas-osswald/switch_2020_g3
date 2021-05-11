a69c4110bad86f37415e274400b874ad867c6729
Bruno Tamames [1201762] <1201762@isep.ipp.pt>
2021-05-11 11:04:18 +0100

    US151 - add email service + assembler

--- src/main/java/switchtwentytwenty/project/dto/assemblers/implassemblers/FamilyDTODomainAssembler.java
+++ src/main/java/switchtwentytwenty/project/dto/assemblers/implassemblers/FamilyDTODomainAssembler.java
@@ -2,9 +2,7 @@ package switchtwentytwenty.project.dto.assemblers.implassemblers;
 
 import org.springframework.stereotype.Component;
 import switchtwentytwenty.project.domain.aggregates.family.Family;
-import switchtwentytwenty.project.domain.valueobject.FamilyID;
 import switchtwentytwenty.project.domain.valueobject.FamilyName;
-import switchtwentytwenty.project.domain.valueobject.PersonID;
 import switchtwentytwenty.project.domain.valueobject.RegistrationDate;
 import switchtwentytwenty.project.dto.family.InputFamilyDTO;
 import switchtwentytwenty.project.dto.family.OutputFamilyDTO;
@@ -13,35 +11,33 @@ import switchtwentytwenty.project.dto.family.OutputFamilyDTO;
 public class FamilyDTODomainAssembler {
 
     /**
-     * Assembler method to create a Family domain object from a DTO.
+     * Assembler method to create a FamilyName value object
      *
      * @param inputFamilyDTO DTO that contains the Family's information
-     * @param familyID       Domain object representing the FamilyID of the Family to be created
-     * @param adminID        Domain object representing the PersonID of the Administrator of the Family to be created
-     * @return Family domain object
+     * @return FamilyName value object
      */
-    @Deprecated
-    public Family toDomain(InputFamilyDTO inputFamilyDTO, FamilyID familyID, PersonID adminID) {
-
-        FamilyName familyName = new FamilyName(inputFamilyDTO.unpackFamilyName());
-
-        RegistrationDate registrationDate = new RegistrationDate(inputFamilyDTO.unpackLocalDate());
-
-        Family family = new Family(familyID, familyName, registrationDate, adminID);
-
-        return family;
-    }
-
     public FamilyName createFamilyName(InputFamilyDTO inputFamilyDTO) {
         FamilyName familyName = new FamilyName((inputFamilyDTO.unpackFamilyName()));
         return familyName;
     }
 
+    /**
+     * Assembler method to create a RegistrationDate value object
+     *
+     * @param inputFamilyDTO DTO that contains the Family's information
+     * @return RegistrationDate value object
+     */
     public RegistrationDate createRegistrationDate(InputFamilyDTO inputFamilyDTO) {
-        RegistrationDate registrationDate = new RegistrationDate(inputFamilyDTO.unpackLocalDate());
+        RegistrationDate registrationDate = new RegistrationDate(inputFamilyDTO.unpackRegistrationDate());
         return registrationDate;
     }
 
+    /**
+     * Assembler Method to create an output DTO from a domain Family Object
+     *
+     * @param family family which will be converted to DTO
+     * @return OutputFamilyDTO - object containing the family name, family administrator and the registration date
+     */
     public OutputFamilyDTO toDTO(Family family) {
         OutputFamilyDTO outputFamilyDTO = new OutputFamilyDTO(family.getName().toString(), family.id().toString(), family.getAdmin().toString(), family.getRegistrationDate().toString());
         return outputFamilyDTO;
--- src/main/java/switchtwentytwenty/project/dto/person/OutputEmailDTO.java
+++ src/main/java/switchtwentytwenty/project/dto/person/OutputEmailDTO.java
@@ -13,22 +13,14 @@ public class OutputEmailDTO extends RepresentationModel<OutputEmailDTO> {
 
     private String email;
 
-    private String emailID;
-
     /**
-     * This DTO receives information from the updated Person in the Persistence and retrieves the added email and the newly created
-     * email ID (Given by the database). Packages all info to be sent back to the Controller to be converted to an External DTO
+     * This DTO receives information from the updated Person in the Persistence and retrieves the added email.
+     * Sends the info back to the Controller to be converted to an External DTO
      * to be placed in the Response Entity.
      */
 
-
-
     public String unpackEmail() {
         return this.email;
-    }
-
-    public String unpackEmailID() {
-        return this.emailID;
     }
 
     @Override
@@ -37,11 +29,11 @@ public class OutputEmailDTO extends RepresentationModel<OutputEmailDTO> {
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         OutputEmailDTO that = (OutputEmailDTO) o;
-        return Objects.equals(email, that.email) && Objects.equals(emailID, that.emailID);
+        return Objects.equals(email, that.email);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), email, emailID);
+        return Objects.hash(super.hashCode(), email);
     }
 }
