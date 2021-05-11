796ccbed4098f9b53399335e66d39b2ad5957d4c
Miguel Loureiro [1181945] <1181945@isep.ipp.pt>
2021-05-11 16:10:29 +0100

    AccountDTODomainAssembler update

--- src/main/java/switchtwentytwenty/project/dto/assemblers/implassemblers/AccountDTODomainAssembler.java
+++ src/main/java/switchtwentytwenty/project/dto/assemblers/implassemblers/AccountDTODomainAssembler.java
@@ -1,40 +1,37 @@
 package switchtwentytwenty.project.dto.assemblers.implassemblers;
 
 import org.springframework.stereotype.Component;
-import switchtwentytwenty.project.domain.aggregates.account.AbAccount;
 import switchtwentytwenty.project.domain.aggregates.account.IAccount;
-import switchtwentytwenty.project.domain.valueobject.AccountType;
 import switchtwentytwenty.project.domain.valueobject.Designation;
-import switchtwentytwenty.project.domain.valueobject.Movement;
-import switchtwentytwenty.project.domain.valueobject.OwnerID;
 import switchtwentytwenty.project.dto.accounts.InputAccountDTO;
 import switchtwentytwenty.project.dto.accounts.OutputAccountDTO;
+import switchtwentytwenty.project.dto.assemblers.iassemblers.IAccountDTODomainAssembler;
 
 @Component
-public class AccountDTODomainAssembler {
-
-   /* public OwnerID ownerIDToDomain(InputAccountDTO inputAccountDTO) {
-        OwnerID ownerID = new OwnerID(inputAccountDTO.unpackOwnerID());
-        return ownerID;
-    }
-
-    public Movement movementToDomain(InputAccountDTO inputAccountDTO) {
-        Movement movement = new Movement(inputAccountDTO.unpackMovement());
-        return movement;
-    }
+public class AccountDTODomainAssembler implements IAccountDTODomainAssembler {
 
     public Designation designationToDomain(InputAccountDTO inputAccountDTO) {
-        Designation designation = new Designation(inputAccountDTO.unpackDesignation());
-        return designation;
+        return new Designation(inputAccountDTO.getDesignation());
     }
 
-    public AccountType accountTypeToDomain(InputAccountDTO inputAccountDTO) {
-        return inputAccountDTO.unpackAccountType();
+    public Integer initialAmmountToDomain(InputAccountDTO inputAccountDTO) {
+        return inputAccountDTO.getInitialAmount();
+    }
 
-    }*/
+    public String currencyToDomain(InputAccountDTO inputAccountDTO) {
+        return inputAccountDTO.getCurrency();
+    }
 
-    public OutputAccountDTO toDTO(AbAccount savedAccount) {
+    public String ownerIDToDomain(InputAccountDTO inputAccountDTO) {
+        return inputAccountDTO.getOwnerID();
+    }
+
+    public String accountTypeToDomain(InputAccountDTO inputAccountDTO) {
+        return inputAccountDTO.getAccountType();
+    }
+
+    @Override
+    public OutputAccountDTO toDTO(IAccount IAccount) {
         return null;
     }
-
 }
