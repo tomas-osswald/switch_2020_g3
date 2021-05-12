34003a43442acb7bed4189f83928d21085a365f7
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-05-12 11:11:03 +0100

    Added tests for FamilyDataDomainAssembler's methods

---
+++ src/test/java/switchtwentytwenty/project/usecaseservices/applicationservices/implappservices/CreateAccountServiceTest.java
@@ -0,0 +1,12 @@
+package switchtwentytwenty.project.usecaseservices.applicationservices.implappservices;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateAccountServiceTest {
+
+    @Test
+    void createAccount() {
+    }
+}
