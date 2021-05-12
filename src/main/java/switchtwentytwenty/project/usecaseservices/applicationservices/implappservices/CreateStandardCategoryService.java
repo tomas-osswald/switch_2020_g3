b9588021108d4ecb462117c6d3ccf63079f8cce5
Miguel Loureiro [1181945] <1181945@isep.ipp.pt>
2021-05-12 23:36:15 +0100

    AccountDTODomainAssembler test not completed and started StandardCategoryService

--- src/main/java/switchtwentytwenty/project/usecaseservices/applicationservices/implappservices/CreateStandardCategoryService.java
+++ src/main/java/switchtwentytwenty/project/usecaseservices/applicationservices/implappservices/CreateStandardCategoryService.java
@@ -1,14 +1,32 @@
 package switchtwentytwenty.project.usecaseservices.applicationservices.implappservices;
 
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import switchtwentytwenty.project.dto.assemblers.implassemblers.CategoryDTODomainAssembler;
 import switchtwentytwenty.project.dto.category.InputCategoryDTO;
 import switchtwentytwenty.project.dto.category.OutputCategoryDTO;
 import switchtwentytwenty.project.usecaseservices.applicationservices.iappservices.ICreateStandardCategoryService;
+import switchtwentytwenty.project.usecaseservices.irepositories.ICategoryRepository;
+
+import java.util.Locale;
 
 @Service
+@NoArgsConstructor
 public class CreateStandardCategoryService implements ICreateStandardCategoryService {
+
+    private CategoryDTODomainAssembler categoryDTODomainAssembler;
+    private ICategoryRepository categoryRepository;
+
+    @Autowired
+    public CreateStandardCategoryService(ICategoryRepository categoryRepository, CategoryDTODomainAssembler categoryDTODomainAssembler) {
+        this.categoryRepository = categoryRepository;
+        this.categoryDTODomainAssembler = categoryDTODomainAssembler;
+    }
+
     @Override
     public OutputCategoryDTO createStandardCategory(InputCategoryDTO inputCategoryDTO) {
+
         return null;
     }
 }
--- src/test/java/switchtwentytwenty/project/dto/assemblers/implassemblers/AccountDTODomainAssemblerTest.java
+++ src/test/java/switchtwentytwenty/project/dto/assemblers/implassemblers/AccountDTODomainAssemblerTest.java
@@ -1,28 +1,85 @@
 package switchtwentytwenty.project.dto.assemblers.implassemblers;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import switchtwentytwenty.project.domain.valueobject.*;
+import switchtwentytwenty.project.dto.accounts.InputAccountDTO;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountDTODomainAssemblerTest {
 
+    AccountDTODomainAssembler accountDTODomainAssembler = new AccountDTODomainAssembler();
+
+    InputAccountDTO inputAccountDTOa = new InputAccountDTO("balelas", BigDecimal.valueOf(10), "EUR", "tonyze@latinlover.com", "CashAccount");
+    InputAccountDTO inputAccountDTOb = new InputAccountDTO("balelas", BigDecimal.valueOf(10), "EUR", "tonyze@latinlover.com", "CreditCardAccount");
+    InputAccountDTO inputAccountDTOc = new InputAccountDTO("balelas", BigDecimal.valueOf(10), "EUR", "tonyze@latinlover.com", "BankAccount");
+    InputAccountDTO inputAccountDTOd = new InputAccountDTO("balelas", BigDecimal.valueOf(10), "EUR", "tonyze@latinlover.com", "BankSavingsAccount");
+
+
     @Test
-    void designationToDomain() {
+    void designationToDomainSuccess() {
+        Designation expected = new Designation("balelas");
+        Designation result = accountDTODomainAssembler.designationToDomain(inputAccountDTOa);
+
+        assertEquals(expected, result);
     }
 
     @Test
-    void initialAmountToDomain() {
+    void initialAmountToDomainSuccess() {
+        Monetary expected = new Monetary("EUR", BigDecimal.valueOf(10));
+        Monetary result = accountDTODomainAssembler.initialAmountToDomain(inputAccountDTOa);
+
+        assertEquals(expected, result);
+
     }
 
     @Test
-    void ownerIDToDomain() {
+    void ownerIDToDomainSuccess() {
+        OwnerID expected = new PersonID("tonyze@latinlover.com");
+        OwnerID result = accountDTODomainAssembler.ownerIDToDomain(inputAccountDTOa);
+
+        assertEquals(expected, result);
     }
 
     @Test
-    void accountTypeToDomain() {
+    void accountTypeCashAccountToDomainSuccess() {
+        String expected = "CashAccount";
+        String result = accountDTODomainAssembler.accountTypeToDomain(inputAccountDTOa);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void accountTypeCreditCardAccountToDomainSuccess() {
+        String expected = "CreditCardAccount";
+        String result = accountDTODomainAssembler.accountTypeToDomain(inputAccountDTOb);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void accountTypeBankAccountToDomainSuccess() {
+        String expected = "BankAccount";
+        String result = accountDTODomainAssembler.accountTypeToDomain(inputAccountDTOc);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void accountTypeBankSavingsAccountToDomainSuccess() {
+        String expected = "BankSavingsAccount";
+        String result = accountDTODomainAssembler.accountTypeToDomain(inputAccountDTOd);
+
+        assertEquals(expected, result);
     }
 
     @Test
     void toDTO() {
     }
-}
+
+   }
