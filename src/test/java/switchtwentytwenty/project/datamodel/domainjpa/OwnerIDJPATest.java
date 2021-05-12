0d59df173806a945ce18f4e8787bb9772c69158b
Manuel Almeida [1201770] <1201770@isep.ipp.pt>
2021-05-13 00:34:57 +0100

    added OwnerIDJPATest and tests for hopefully full coverage

---
+++ src/test/java/switchtwentytwenty/project/datamodel/domainjpa/OwnerIDJPATest.java
@@ -0,0 +1,98 @@
+package switchtwentytwenty.project.datamodel.domainjpa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerIDJPATest {
+    String personOwnerID = "email@email.com";
+    String familyOwnerID = "@tonyZe@lationlover.com";
+    
+    @Test
+    void getOwnerID() {
+        String expected = "email@email.com";
+
+        OwnerIDJPA ownerIDJPA = new OwnerIDJPA(personOwnerID);
+
+        String result = ownerIDJPA.toString();
+
+        assertEquals(expected, result);
+    }
+    @Test
+    void setOwnerID() {
+        OwnerIDJPA ownerIDJPA = new OwnerIDJPA();
+        ownerIDJPA.setOwnerID(personOwnerID);
+
+        String expected = "email@email.com";
+        String result = ownerIDJPA.getOwnerID();
+
+        assertEquals(expected, result);
+    }
+    
+    @Test
+    void equalsTestSameOwnerIDJPA() {
+        OwnerIDJPA ownerIDJPAOne = new OwnerIDJPA(personOwnerID);
+        OwnerIDJPA ownerIDJPATwo = ownerIDJPAOne;
+
+        assertEquals(ownerIDJPAOne, ownerIDJPATwo);
+    }
+
+    @Test
+    void equalsTestEqualOwnerIDJPAs() {
+        OwnerIDJPA ownerIDJPAOne = new OwnerIDJPA(personOwnerID);
+        OwnerIDJPA ownerIDJPATwo = new OwnerIDJPA(personOwnerID);
+
+        assertEquals(ownerIDJPAOne, ownerIDJPATwo);
+        assertNotSame(ownerIDJPAOne, ownerIDJPATwo);
+    }
+    @Test
+    void equalsTestEqualOwnerIDJPAsFromFamilyID() {
+        OwnerIDJPA ownerIDJPAOne = new OwnerIDJPA(familyOwnerID);
+        OwnerIDJPA ownerIDJPATwo = new OwnerIDJPA(familyOwnerID);
+
+        assertEquals(ownerIDJPAOne, ownerIDJPATwo);
+        assertNotSame(ownerIDJPAOne, ownerIDJPATwo);
+    }
+
+
+    @Test
+    void equalsTestDifferentOwnerIDJPAs() {
+        OwnerIDJPA ownerIDJPAOne = new OwnerIDJPA(personOwnerID);
+        OwnerIDJPA ownerIDJPATwo = new OwnerIDJPA("test@gmail.com");
+
+        assertNotEquals(ownerIDJPAOne, ownerIDJPATwo);
+    }
+
+    @Test
+    void equalsTestDifferentObjects() {
+        OwnerIDJPA ownerIDJPA = new OwnerIDJPA(personOwnerID);
+
+        assertNotEquals(ownerIDJPA, personOwnerID);
+    }
+
+    @Test
+    void equalsTestDifferentFromNull() {
+        OwnerIDJPA ownerIDJPA = new OwnerIDJPA(personOwnerID);
+        String nullString = null;
+
+        assertNotEquals(ownerIDJPA, nullString);
+    }
+
+    @Test
+    void hashCodeSameHashCode(){
+        OwnerIDJPA ownerIDJPAOne = new OwnerIDJPA(personOwnerID);
+        OwnerIDJPA ownerIDJPATwo = new OwnerIDJPA(personOwnerID);
+
+        assertEquals(ownerIDJPAOne.hashCode(), ownerIDJPATwo.hashCode());
+        assertNotSame(ownerIDJPAOne, ownerIDJPATwo);
+    }
+
+    @Test
+    void hashCodeDifferentHashCode(){
+        OwnerIDJPA ownerIDJPAOne = new OwnerIDJPA(personOwnerID);
+        OwnerIDJPA ownerIDJPATwo = new OwnerIDJPA("test@gmail.com");
+
+        assertNotEquals(ownerIDJPAOne, ownerIDJPATwo);
+    }
+
+}
