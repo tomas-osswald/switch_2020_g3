04d9ec1aee451089e60a84c159ea101567b39d5d
João Pinto [1201765] <1201765@isep.ipp.pt>
2021-05-12 15:43:28 +0100

    Update FamilyRESTController tests

---
+++ src/test/java/switchtwentytwenty/project/interfaceadapters/controller/implcontrollers/FamilyRESTControllerIT.java
@@ -0,0 +1,98 @@
+package switchtwentytwenty.project.interfaceadapters.controller.implcontrollers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.hateoas.Link;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import switchtwentytwenty.project.dto.OptionsDTO;
+import switchtwentytwenty.project.dto.family.AddFamilyAndSetAdminDTO;
+import switchtwentytwenty.project.dto.family.AddRelationDTO;
+import switchtwentytwenty.project.dto.family.OutputFamilyDTO;
+import switchtwentytwenty.project.interfaceadapters.controller.icontrollers.IFamilyRESTController;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class FamilyRESTControllerIT {
+    AddFamilyAndSetAdminDTO dto = new AddFamilyAndSetAdminDTO("tony@email.com", "Silva", "12/12/1222", 999999999, 919999999, "Rua", "Cidade", "12B", "4400-123", "Silva", "12/12/2000");
+    AddFamilyAndSetAdminDTO invaliddto = new AddFamilyAndSetAdminDTO("tonyemail.com", "Silva", "12/12/1222", 999999999, 919999999, "Rua", "Cidade", "12B", "4400-123", "Silva", "12/12/2000");
+
+    @Autowired
+    IFamilyRESTController familyRESTController;
+
+    private AutoCloseable closeable;
+
+    @BeforeEach
+    void setUp() {
+        // We can create mock based on the Interface or a Class
+        // templateEngine = mock(TemplateEngine.class);
+
+        // init mock with annotations
+        //MockitoAnnotations.initMocks(this);
+
+        // just another version of initilization of mocks with annotation
+        // pay attention to tear down method - we have to call close method
+        closeable = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        closeable.close();
+    }
+
+
+    @Test
+    void testCreateFamilyAndSetAdmin() {
+        ResponseEntity expected = new ResponseEntity("Business Error Api Logic", HttpStatus.UNPROCESSABLE_ENTITY);
+        ResponseEntity result = familyRESTController.createFamilyAndSetAdmin(invaliddto);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void familiesOptionsTest() {
+        OptionsDTO optionsDTO = new OptionsDTO();
+        Link link = linkTo(methodOn(FamilyRESTController.class).familiesOptions()).withRel("POST - Add New Family");
+        optionsDTO.add(link);
+
+        HttpHeaders header = new HttpHeaders();
+        header.set("Allow", "POST, OPTIONS");
+
+        ResponseEntity expected = new ResponseEntity<>(optionsDTO, header, HttpStatus.OK);
+        ResponseEntity result = familyRESTController.familiesOptions();
+
+        assertEquals(result, expected);
+    }
+
+
+    @Test
+    void addRelationTest() {
+        AddRelationDTO addRelationDTO = new AddRelationDTO();
+
+        assertThrows(UnsupportedOperationException.class, () -> familyRESTController.addRelation(addRelationDTO, "@tonize@gmail.com"));
+    }
+
+    @Test
+    void getFamilyNameTest() {
+        String familyName = "Silva";
+
+        assertThrows(UnsupportedOperationException.class, () -> familyRESTController.getFamily(familyName));
+    }
+
+    @Test
+    void getFamilyOptionsTest() {
+
+    }
+}
