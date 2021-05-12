78a8a5387338c316d3b9fb7ece4b150381526aea
Manuel Almeida [1201770] <1201770@isep.ipp.pt>
2021-05-12 14:57:21 +0100

    corrected Designation equals method repetition

--- src/main/java/switchtwentytwenty/project/domain/valueobject/Designation.java
+++ src/main/java/switchtwentytwenty/project/domain/valueobject/Designation.java
@@ -6,19 +6,6 @@ public class Designation {
 
     private String designation;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Designation that = (Designation) o;
-        return Objects.equals(designation, that.designation);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(designation);
-    }
-
     public Designation(String designation) {
         this.designation = designation;
     }
