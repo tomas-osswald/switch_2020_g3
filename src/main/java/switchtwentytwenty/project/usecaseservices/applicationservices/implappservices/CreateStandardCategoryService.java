e04214238c316877ddd372a56c1aaa34cb763e28
Miguel Loureiro [1181945] <1181945@isep.ipp.pt>
2021-05-13 00:02:22 +0100

    fixed error in CreatedStandardCategoryService

--- src/main/java/switchtwentytwenty/project/usecaseservices/applicationservices/implappservices/CreateStandardCategoryService.java
+++ src/main/java/switchtwentytwenty/project/usecaseservices/applicationservices/implappservices/CreateStandardCategoryService.java
@@ -9,8 +9,6 @@ import switchtwentytwenty.project.dto.category.OutputCategoryDTO;
 import switchtwentytwenty.project.usecaseservices.applicationservices.iappservices.ICreateStandardCategoryService;
 import switchtwentytwenty.project.usecaseservices.irepositories.ICategoryRepository;
 
-import java.util.Locale;
-
 @Service
 @NoArgsConstructor
 public class CreateStandardCategoryService implements ICreateStandardCategoryService {
@@ -18,16 +16,9 @@ public class CreateStandardCategoryService implements ICreateStandardCategorySer
     private CategoryDTODomainAssembler categoryDTODomainAssembler;
     private ICategoryRepository categoryRepository;
 
-    @Autowired
-    public CreateStandardCategoryService(ICategoryRepository categoryRepository, CategoryDTODomainAssembler categoryDTODomainAssembler) {
-        this.categoryRepository = categoryRepository;
-        this.categoryDTODomainAssembler = categoryDTODomainAssembler;
-    }
-
     @Override
     public OutputCategoryDTO createStandardCategory(InputCategoryDTO inputCategoryDTO) {
         throw new UnsupportedOperationException();
 
-        return null;
     }
 }
