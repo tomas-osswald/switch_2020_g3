24dd8eed4b72968b853469eaa10920b3d6a458f1
Manuel Almeida [1201770] <1201770@isep.ipp.pt>
2021-05-13 11:34:52 +0100

    Added OutputAccountDTOTest and test for full coverage

--- src/main/java/switchtwentytwenty/project/dto/accounts/OutputAccountDTO.java
+++ src/main/java/switchtwentytwenty/project/dto/accounts/OutputAccountDTO.java
@@ -1,17 +1,16 @@
 package switchtwentytwenty.project.dto.accounts;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
+import java.util.Objects;
 
 @Setter
 @Getter
 @NoArgsConstructor
 public class OutputAccountDTO extends RepresentationModel {
 
-    //Criado para não "partir" o getAccountID() do Rest Controller antes de juntar todas as partes.
     private String accountID;
     private String ownerID;
     private String designation;
@@ -20,5 +19,19 @@ public class OutputAccountDTO extends RepresentationModel {
         this.accountID = accountID;
         this.ownerID = ownerID;
         this.designation = designation;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OutputAccountDTO)) return false;
+        if (!super.equals(o)) return false;
+        OutputAccountDTO that = (OutputAccountDTO) o;
+        return Objects.equals(accountID, that.accountID) && Objects.equals(ownerID, that.ownerID) && Objects.equals(designation, that.designation);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), accountID, ownerID, designation);
     }
 }
