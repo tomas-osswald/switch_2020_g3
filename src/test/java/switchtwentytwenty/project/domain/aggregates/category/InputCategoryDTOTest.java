f6341a8a19e1ef1f6f3a196554c3fa164a3d52b8
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-05-13 10:26:10 +0100

    Coverage tests for CategoryRepository and OutputCategoryDTO

---
+++ src/test/java/switchtwentytwenty/project/domain/aggregates/category/InputCategoryDTOTest.java
@@ -0,0 +1,40 @@
+package switchtwentytwenty.project.domain.aggregates.category;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputCategoryDTOTest {
+
+    @Test
+    void getCategoryID() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getParentID() {
+    }
+
+    @Test
+    void getFamilyID() {
+    }
+
+    @Test
+    void setCategoryID() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void setParentID() {
+    }
+
+    @Test
+    void setFamilyID() {
+    }
+}
