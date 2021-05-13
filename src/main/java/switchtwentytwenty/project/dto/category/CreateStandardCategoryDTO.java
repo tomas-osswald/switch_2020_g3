59bd0ad2e0db95aa878d89fef65416b83e4cc8d1
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-05-13 11:20:48 +0100

    Coverage tests for CreateStandardCategoryDTO

--- src/main/java/switchtwentytwenty/project/dto/category/CreateStandardCategoryDTO.java
+++ src/main/java/switchtwentytwenty/project/dto/category/CreateStandardCategoryDTO.java
@@ -1,4 +1,21 @@
 package switchtwentytwenty.project.dto.category;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Setter
+@Getter
 public class CreateStandardCategoryDTO {
+
+    private String categoryDescription;
+    private Long categoryID;
+    private Long parentID;
+
+    public CreateStandardCategoryDTO(String categoryDescription, Long categoryID, Long parentID) {
+        this.categoryDescription = categoryDescription;
+        this.categoryID = categoryID;
+        this.parentID = parentID;
+    }
 }
