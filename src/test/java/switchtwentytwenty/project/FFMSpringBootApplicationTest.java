bafc5e8bb779bde233927e6a91fe63d0360fe14a
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-05-13 22:13:55 +0100

    test for main method

---
+++ src/test/java/switchtwentytwenty/project/FFMSpringBootApplicationTest.java
@@ -0,0 +1,12 @@
+package switchtwentytwenty.project;
+
+import org.junit.jupiter.api.Test;
+
+class FFMSpringBootApplicationTest {
+// Test class added ONLY to cover main() invocation not covered by application tests.
+
+    @Test
+    void main() {
+        FFMSpringBootApplication.main(new String[]{});
+    }
+}
