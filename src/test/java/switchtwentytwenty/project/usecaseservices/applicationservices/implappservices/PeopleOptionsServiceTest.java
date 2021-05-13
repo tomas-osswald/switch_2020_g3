aca75de4a7dfecbcbc3ba0a1d2a76e2a050e1faa
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-05-13 20:40:48 +0100

    PeopleOptionsService Test

---
+++ src/test/java/switchtwentytwenty/project/usecaseservices/applicationservices/implappservices/PeopleOptionsServiceTest.java
@@ -0,0 +1,33 @@
+package switchtwentytwenty.project.usecaseservices.applicationservices.implappservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.hateoas.Link;
+import switchtwentytwenty.project.dto.OptionsDTO;
+import switchtwentytwenty.project.interfaceadapters.controller.implcontrollers.PersonRESTController;
+import switchtwentytwenty.project.usecaseservices.applicationservices.iappservices.IPeopleOptionsService;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@SpringBootTest
+class PeopleOptionsServiceTest {
+
+    @Autowired
+    IPeopleOptionsService peopleOptionsService;
+
+    @Test
+    void getPeopleOptions() {
+        OptionsDTO expected = new OptionsDTO();
+        Link linkToPeopleOptions = linkTo(methodOn(PersonRESTController.class).peopleOptions()).withSelfRel();
+        expected.add(linkToPeopleOptions);
+
+        OptionsDTO result = peopleOptionsService.getPeopleOptions();
+
+        assertNotNull(result);
+        assertEquals(expected,result);
+        assertNotSame(expected,result);
+    }
+}
