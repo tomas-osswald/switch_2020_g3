6f705d9f6ca15a41fe72d421540c694c0c467b2b
João Pinto [1201765] <1201765@isep.ipp.pt>
2021-05-13 21:53:40 +0100

    Update to PeopleOptionsService, added link (POST)

--- src/test/java/switchtwentytwenty/project/usecaseservices/applicationservices/implappservices/PeopleOptionsServiceTest.java
+++ src/test/java/switchtwentytwenty/project/usecaseservices/applicationservices/implappservices/PeopleOptionsServiceTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.Link;
 import switchtwentytwenty.project.dto.OptionsDTO;
+import switchtwentytwenty.project.dto.person.AddFamilyMemberDTO;
 import switchtwentytwenty.project.interfaceadapters.controller.implcontrollers.PersonRESTController;
 import switchtwentytwenty.project.usecaseservices.applicationservices.iappservices.IPeopleOptionsService;
 
@@ -22,7 +23,9 @@ class PeopleOptionsServiceTest {
     void getPeopleOptions() {
         OptionsDTO expected = new OptionsDTO();
         Link linkToPeopleOptions = linkTo(methodOn(PersonRESTController.class).peopleOptions()).withSelfRel();
+        Link linkToPOST = linkTo(methodOn(PersonRESTController.class).addFamilyMember(new AddFamilyMemberDTO())).withRel("POST - Add Family Member");
         expected.add(linkToPeopleOptions);
+        expected.add(linkToPOST);
 
         OptionsDTO result = peopleOptionsService.getPeopleOptions();
 
