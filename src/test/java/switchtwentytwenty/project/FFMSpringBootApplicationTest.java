a46dfc2b8c371568d911783944a4f6705aac5c46
João Pinto [1201765] <1201765@isep.ipp.pt>
2021-05-19 11:07:39 +0100

    Add test for AccountAlreadyRegistredExcpetion and changes to pom.xml to exclude FFMSpringBootApplicationTest

--- src/test/java/switchtwentytwenty/project/FFMSpringBootApplicationTest.java
+++ src/test/java/switchtwentytwenty/project/FFMSpringBootApplicationTest.java
@@ -2,7 +2,7 @@ package switchtwentytwenty.project;
 
 import org.junit.jupiter.api.Test;
 
-/*
+
 class FFMSpringBootApplicationTest {
 // Test class added ONLY to cover main() invocation not covered by application tests.
 
@@ -10,4 +10,4 @@ class FFMSpringBootApplicationTest {
     void main() {
         FFMSpringBootApplication.main(new String[]{});
     }
-}*/
+}
