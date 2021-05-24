8ca7a45cc831f513a39edd59d831999af3296101
João Pinto [1201765] <1201765@isep.ipp.pt>
2021-05-24 18:05:42 +0100

    Add compareTo() to Relation.class

--- src/main/java/switchtwentytwenty/project/domain/valueobject/Relation.java
+++ src/main/java/switchtwentytwenty/project/domain/valueobject/Relation.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 import java.util.Objects;
 
 @AllArgsConstructor
-public class Relation implements ValueObject {
+public class Relation implements ValueObject, Comparable<Relation> {
     private final PersonID memberA;
     private final PersonID memberB;
     private final RelationDesignation relationDesignation;
@@ -33,5 +33,14 @@ public class Relation implements ValueObject {
     @Override
     public int hashCode() {
         return Objects.hash(memberA, memberB);
+    }
+
+    //TODO: rever isto sff, Johny Sins
+    @Override
+    public int compareTo(Relation o) {
+        int result = -1;
+        if (this.memberA.equals(o.memberB) && this.memberB.equals(o.memberA)) result = 0;
+        if (this.memberA.equals(o.memberA) && this.memberB.equals(o.memberB)) result = 0;
+        return result;
     }
 }
