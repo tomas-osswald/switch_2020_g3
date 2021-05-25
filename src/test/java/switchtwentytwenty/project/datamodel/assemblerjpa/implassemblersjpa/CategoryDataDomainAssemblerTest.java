75e4e2e2b78f37e872d37364185a026fadac5910
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-05-25 16:06:03 +0100

    CategoryDataDomainAssemblerTest

--- src/test/java/switchtwentytwenty/project/datamodel/assemblerjpa/implassemblersjpa/CategoryDataDomainAssemblerTest.java
+++ src/test/java/switchtwentytwenty/project/datamodel/assemblerjpa/implassemblersjpa/CategoryDataDomainAssemblerTest.java
@@ -3,28 +3,53 @@ package switchtwentytwenty.project.datamodel.assemblerjpa.implassemblersjpa;
 import org.junit.jupiter.api.Test;
 import switchtwentytwenty.project.datamodel.domainjpa.CategoryJPA;
 import switchtwentytwenty.project.datamodel.domainjpa.FamilyIDJPA;
+import switchtwentytwenty.project.domain.valueobject.CategoryID;
+import switchtwentytwenty.project.domain.valueobject.CategoryName;
+import switchtwentytwenty.project.domain.valueobject.FamilyID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
 
 
 class CategoryDataDomainAssemblerTest {
 
     CategoryDataDomainAssembler categoryDataDomainAssembler = new CategoryDataDomainAssembler();
     CategoryJPA customCategoryJPA = new CategoryJPA("catName", 12L, 11L, new FamilyIDJPA("@famid@famid.com"));
-    CategoryJPA standardCategoryJPA = new CategoryJPA("catName", 12L, 11L, null);
 
 
     @Test
     void createCategoryID() {
+        CategoryID expected = new CategoryID(12L);
+        CategoryID result = categoryDataDomainAssembler.createCategoryID(customCategoryJPA);
+
+        assertNotSame(expected, result);
+        assertEquals(expected, result);
     }
 
     @Test
     void createCategoryName() {
+        CategoryName expected = new CategoryName("catName");
+        CategoryName result = categoryDataDomainAssembler.createCategoryName(customCategoryJPA);
+
+        assertNotSame(expected, result);
+        assertEquals(expected, result);
     }
 
     @Test
     void createParentID() {
+        CategoryID expected = new CategoryID(11L);
+        CategoryID result = categoryDataDomainAssembler.createParentID(customCategoryJPA);
+
+        assertNotSame(expected, result);
+        assertEquals(expected, result);
     }
 
     @Test
     void createFamilyID() {
+        FamilyID expected = new FamilyID("@famid@famid.com");
+        FamilyID result = categoryDataDomainAssembler.createFamilyID(customCategoryJPA);
+
+        assertNotSame(expected, result);
+        assertEquals(expected, result);
     }
 }
