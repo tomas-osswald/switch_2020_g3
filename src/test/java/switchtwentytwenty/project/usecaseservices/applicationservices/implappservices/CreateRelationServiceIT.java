51ebeb4b5c433fcd0ddaaabed1f428e0f4b2315e
Manuel Almeida [1201770] <1201770@isep.ipp.pt>
2021-05-27 14:45:52 +0100

    added CreateRelationServiceIT

---
+++ src/test/java/switchtwentytwenty/project/usecaseservices/applicationservices/implappservices/CreateRelationServiceIT.java
@@ -0,0 +1,58 @@
+package switchtwentytwenty.project.usecaseservices.applicationservices.implappservices;
+
+
+import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import switchtwentytwenty.project.datamodel.assemblerjpa.implassemblersjpa.FamilyDataDomainAssembler;
+import switchtwentytwenty.project.datamodel.repositoryjpa.IFamilyRepositoryJPA;
+import switchtwentytwenty.project.domain.aggregates.family.Family;
+import switchtwentytwenty.project.domain.valueobject.FamilyID;
+import switchtwentytwenty.project.domain.valueobject.Relation;
+import switchtwentytwenty.project.domain.valueobject.RelationID;
+import switchtwentytwenty.project.dto.assemblers.implassemblers.FamilyDTODomainAssembler;
+import switchtwentytwenty.project.dto.family.InputRelationDTO;
+import switchtwentytwenty.project.dto.family.OutputRelationDTO;
+import switchtwentytwenty.project.usecaseservices.irepositories.IFamilyRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreateRelationServiceIT {
+    CreateRelationService createRelationService;
+
+    @Mock
+    FamilyDataDomainAssembler familyDataDomainAssembler;
+
+    @Mock
+    IFamilyRepositoryJPA familyRepositoryJPA;
+
+    @InjectMocks
+    IFamilyRepository familyRepository;
+
+    @Autowired
+    FamilyDTODomainAssembler familyDTODomainAssembler;
+
+
+    String personIDOne = "tonyze@latinlover.com";
+    String personIDTwo = "mariaze@latinlover.com";
+    String designation = "amante";
+    String familyID = "@tonyze@latinlover.com";
+    InputRelationDTO inputRelationDTO = new InputRelationDTO(personIDOne, personIDTwo, designation, familyID);
+
+    @Disabled
+    @DisplayName("Successfully create a relation using valid data.")
+    @Test
+    void createRelationServiceSuccess() {
+
+    }
+}
