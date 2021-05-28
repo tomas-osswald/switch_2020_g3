f9fcc380e4207355d1722da2448c428b88efde62
Miguel Loureiro [1181945] <1181945@isep.ipp.pt>
2021-05-28 17:50:15 +0100

    update CreateRelationDTO test

--- src/test/java/switchtwentytwenty/project/dto/family/CreateRelationDTOTest.java
+++ src/test/java/switchtwentytwenty/project/dto/family/CreateRelationDTOTest.java
@@ -10,21 +10,27 @@ class CreateRelationDTOTest {
     @Test
     void getMemberOneIDMustNotBeNull() {
         CreateRelationDTO createRelationDTO = new CreateRelationDTO("tonyze", "katia", "BFF");
+        String expected = "tonyze";
         String result = createRelationDTO.getMemberOneID();
+        assertEquals(expected, result);
         assertNotNull(result);
     }
 
     @Test
     void getMemberTwoID() {
         CreateRelationDTO createRelationDTO = new CreateRelationDTO("tonyze", "katia", "BFF");
+        String expected = "katia";
         String result = createRelationDTO.getMemberTwoID();
+        assertEquals(expected, result);
         assertNotNull(result);
     }
 
     @Test
     void getRelationDesignation() {
         CreateRelationDTO createRelationDTO = new CreateRelationDTO("tonyze", "katia", "BFF");
+        String expected = "BFF";
         String result = createRelationDTO.getRelationDesignation();
+        assertEquals(expected, result);
         assertNotNull(result);
     }
 
