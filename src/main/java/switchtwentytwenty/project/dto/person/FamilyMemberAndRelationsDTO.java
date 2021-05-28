7b2612680b695a65d0a532a1324da025f4dc61ed
Diogo Ribeiro [1201768] <1201768@isep.ipp.pt>
2021-05-28 17:50:55 +0100

    add tests to FamilyMemberAndRelationsDTO

--- src/main/java/switchtwentytwenty/project/dto/person/FamilyMemberAndRelationsDTO.java
+++ src/main/java/switchtwentytwenty/project/dto/person/FamilyMemberAndRelationsDTO.java
@@ -8,6 +8,7 @@ import org.springframework.hateoas.RepresentationModel;
 import switchtwentytwenty.project.dto.family.OutputRelationDTO;
 
 import java.util.List;
+import java.util.Objects;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,4 +19,18 @@ public class FamilyMemberAndRelationsDTO extends RepresentationModel<FamilyMembe
     private String name;
     private String personID;
     private List<OutputRelationDTO> relations;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FamilyMemberAndRelationsDTO)) return false;
+        if (!super.equals(o)) return false;
+        FamilyMemberAndRelationsDTO that = (FamilyMemberAndRelationsDTO) o;
+        return Objects.equals(getName(), that.getName()) && Objects.equals(getPersonID(), that.getPersonID()) && Objects.equals(getRelations(), that.getRelations());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getName(), getPersonID(), getRelations());
+    }
 }
