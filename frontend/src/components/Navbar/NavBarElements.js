681e224ba836a4dd3494b7c0f7d05c531d264f08
Diogo Ribeiro [1201768] <1201768@isep.ipp.pt>
2021-05-31 13:14:54 +0100

    add styled components to JS

---
+++ frontend/src/components/Navbar/NavBarElements.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+// Usando Styled Components, o naming desta styled.div não pode ser o mesmo do Componente
+// em JS visto que no momento do import na classe correspondente não causa overflow
+export const NavbarDiv = styled.div`
+
+  width: 25%;
+  heigth: vh;
+  float: left;
+
+`
---
+++ frontend/src/components/Navbar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {NavbarDiv} from "./NavBarElements";
+
+
+function Navbar(){
+    return (
+        <NavbarDiv>
+            Navbar here!!!
+        </NavbarDiv>
+    )
+}
+
+export default Navbar;
