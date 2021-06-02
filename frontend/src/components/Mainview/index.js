f062efbce7803c62dc1acbbad211388816f40017
João Pinto [1201765] <1201765@isep.ipp.pt>
2021-06-02 16:28:24 +0100

    Add Routes and rafactored frontend packaging

--- frontend/src/components/Mainview/index.js
+++ frontend/src/components/Mainview/index.js
@@ -1,26 +1,23 @@
 import React, {useContext} from 'react';
-import {MainviewDiv} from "./MainviewElements";
-import logo from "../../logo.svg";
-import Table from "../Table";
 import AppContext from "../../context/AppContext";
-import Navbar from "../Navbar";
+import {MainviewDiv} from "./MainviewElements";
 
 
-function Mainview(){
-    const { state, dispatch } = useContext(AppContext);
-    const { name, mainView } = state;
-    const { data } = name;
+function Mainview() {
+    const {state, dispatch} = useContext(AppContext);
+    const {name, mainView} = state;
+    const {data} = name;
 
-    if(mainView === 'profile') {
-        return(
+    if (mainView === 'profile') {
+        return (
             <MainviewDiv>
                 Mainview deste lado
                 <br/>
                 Profile aqui
             </MainviewDiv>
         )
-    } else if (mainView === 'createFamily'){
-        return(
+    } else if (mainView === 'createFamily') {
+        return (
             <MainviewDiv>
                 Mainview deste lado
                 <br/>
@@ -28,7 +25,7 @@ function Mainview(){
             </MainviewDiv>
         )
     } else if (mainView === 'family') {
-        return(
+        return (
             <MainviewDiv>
                 Mainview deste lado
                 <br/>
