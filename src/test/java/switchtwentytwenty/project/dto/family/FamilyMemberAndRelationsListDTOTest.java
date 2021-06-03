2b40c703e42dec9a8a0f0eefe2cf95d57cb4fb74
Ricardo Batista [1201778] <1201778@isep.ipp.pt>
2021-06-03 19:40:55 +0100

    Killing mutants in FamilyMemberAndRelationsListDTO

--- src/test/java/switchtwentytwenty/project/dto/family/FamilyMemberAndRelationsListDTOTest.java
+++ src/test/java/switchtwentytwenty/project/dto/family/FamilyMemberAndRelationsListDTOTest.java
@@ -6,8 +6,7 @@ import switchtwentytwenty.project.dto.person.FamilyMemberAndRelationsDTO;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class FamilyMemberAndRelationsListDTOTest {
 
@@ -33,5 +32,76 @@ class FamilyMemberAndRelationsListDTOTest {
 
         assertNotNull(result);
         assertEquals(familyMemberAndRelationsDTO, result);
+    }
+
+    @Test
+    void testEquals_expectingNotEquals() {
+        List<FamilyMemberAndRelationsDTO> expected = new ArrayList<>();
+        FamilyMemberAndRelationsDTO dto = new FamilyMemberAndRelationsDTO();
+        expected.add(dto);
+
+        List<FamilyMemberAndRelationsDTO> result = new ArrayList<>();
+
+        assertNotEquals(expected, result);
+    }
+
+    @Test
+    void testEqualsWithNullObjectExpectingNotEquals() {
+        List<FamilyMemberAndRelationsDTO> expected = new ArrayList<>();
+        FamilyMemberAndRelationsDTO dto = new FamilyMemberAndRelationsDTO();
+        expected.add(dto);
+
+        List<FamilyMemberAndRelationsDTO> result = null;
+
+        assertNotEquals(expected, result);
+    }
+
+    @Test
+    void testEqualsCompareWithStringExpectingNotEquals() {
+        List<FamilyMemberAndRelationsDTO> expected = new ArrayList<>();
+        FamilyMemberAndRelationsDTO dto = new FamilyMemberAndRelationsDTO();
+        expected.add(dto);
+
+        String result = "3";
+
+        assertNotEquals(expected, result);
+    }
+
+    @Test
+    void testSameHashCodeExpectingEquals() {
+        List<FamilyMemberAndRelationsDTO> expected = new ArrayList<>();
+        FamilyMemberAndRelationsDTO dto = new FamilyMemberAndRelationsDTO();
+        expected.add(dto);
+
+        List<FamilyMemberAndRelationsDTO> result = new ArrayList<>();
+        result.add(dto);
+
+        assertEquals(expected.hashCode(), result.hashCode());
+    }
+
+    @Test
+    void testDifferentHashCodeExpectingNotEquals() {
+        List<FamilyMemberAndRelationsDTO> expected = new ArrayList<>();
+        FamilyMemberAndRelationsDTO dto = new FamilyMemberAndRelationsDTO("name", "personID", null);
+        expected.add(dto);
+
+        List<FamilyMemberAndRelationsDTO> result = new ArrayList<>();
+
+
+        assertNotEquals(expected.hashCode(), result.hashCode());
+    }
+
+    @Test
+    void testDifferentHashCodeExpectingNotEqualsAgain() {
+        List<FamilyMemberAndRelationsDTO> expected = new ArrayList<>();
+        FamilyMemberAndRelationsDTO dto = new FamilyMemberAndRelationsDTO("name", "personID", null);
+        expected.add(dto);
+
+        List<FamilyMemberAndRelationsDTO> result = new ArrayList<>();
+        FamilyMemberAndRelationsDTO dtoNull = new FamilyMemberAndRelationsDTO();
+        result.add(dtoNull);
+
+
+        assertNotEquals(expected.hashCode(), result.hashCode());
     }
 }
