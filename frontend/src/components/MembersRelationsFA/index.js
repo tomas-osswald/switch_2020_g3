855f75083129974f75c42d21458b03ad6167b1f1
Diogo Ribeiro [1201768] <1201768@isep.ipp.pt>
2021-06-04 11:58:42 +0100

    add table for relations

--- frontend/src/components/MembersRelationsFA/index.js
+++ frontend/src/components/MembersRelationsFA/index.js
@@ -1,6 +1,7 @@
 import React, {useContext, useEffect} from 'react';
 import AppContext from "../../context/AppContext";
 import {fetchFamilyRelationsFA} from "../../context/Actions";
+import {Table} from 'react-bootstrap';
 import {func} from "prop-types";
 
 
@@ -19,14 +20,45 @@ function MembersRelationsFA() {
 
         fetchFamilyRelationsFA(dispatch, familyId);
 
-
-
     }, [])
 
     function buildInnerText(){
         let html = familyMemberAndRelationsDTO.map((row,index) => {
             let relassoes = row.relations.map((relationsRow,relationsIndex)=>{
-                return (<tr key={relationsIndex}> {relationsRow.memberTwoID} : {relationsRow.relationDesignation} </tr>)
+                return (
+                    <div>
+                        <tr key={relationsIndex}> {relationsRow.memberTwoID} : {relationsRow.relationDesignation} </tr>
+                        <Table striped bordered hover variant="dark">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Username</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td key={relationsIndex}>1</td>
+                                <td>{}</td>
+                                <td>{relationsRow.relationDesignation}</td>
+                                <td>{relationsRow.memberTwoID}</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td colSpan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                            </tbody>
+                        </Table>
+                    </div>
+                )
             })
             return (<tr key={index}>Name: {row.name} Email: {row.personID} Relações : {relassoes}  </tr>)});
 
@@ -51,10 +83,7 @@ function MembersRelationsFA() {
         } else {
             return (
                 <div>
-
                     <p>{buildInnerText()}</p>
-
-
                 </div>
             )
         }
