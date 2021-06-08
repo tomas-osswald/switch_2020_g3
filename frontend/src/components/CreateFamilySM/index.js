d6406ff5163768ee9e700652806fee9b235ce7e2
Ricardo Batista [1201778] <1201778@isep.ipp.pt>
2021-06-08 14:47:14 +0100

    Updated Component and added conditionals
    Co-Authored-by: Manuel Almeida <1201770@isep.ipp.pt>

--- frontend/src/components/CreateFamilySM/index.js
+++ frontend/src/components/CreateFamilySM/index.js
@@ -35,6 +35,8 @@ export default function CreateFamilySM() {
     const [registrationDate, setRegistrationDate] = useState('');
 
 
+    const [display, setDisplay] = useState(true);
+
     // [] -> Vazio, quando o componente é montado (1ª vez)
     // sem o [] -> sempre que há um Update qualquer ao próprio componente
     // [aqui dentro definir o que vai fazer com que seja feito update] -> no caso de apenas querer modificar estado quando um campo específico é mudado.
@@ -51,46 +53,83 @@ export default function CreateFamilySM() {
             registrationDate: registrationDate
         }
         createFamilySM(dispatch, createFamily)
+
+    }
+
+    function changeDisplay() {
+        setDisplay(!display)
+    }
+
+    function submitAndChangeDisplay(){
+        handleSubmit();
+        changeDisplay();
+    }
+
+    //target.value -> permite que cada valor introduzido seja acrescentado ao valor anterior. Se for apagado algum dos valores, altera também o estado atual da variável.
+    // Consultar Manuel Almeida
+
+    if (display) {
+        return (
+            <div>
+                <form>
+                    <label>email:</label>
+                    <input type="text" id="emailID" onChange={emailID => setEmailID(emailID.target.value)}/>
+                    <label>admin name:</label>
+                    <input type="text" id="name" onChange={name => setName(name.target.value)}/>
+                    <label>birthdate:</label>
+                    <input type="text" id="birthdate" onChange={birthdate => setBirthdate(birthdate.target.value)}/>
+                    <label>Vat Number:</label>
+                    <input type="text" id="vatnumber" onChange={vatNumber => setVatNumber(vatNumber.target.value)}/>
+                    <label>phone:</label>
+                    <input type="text" id="phone" onChange={phone => setPhoneNumber(phone.target.value)}/>
+                    <label>street:</label>
+                    <input type="text" id="street" onChange={street => setStreet(street.target.value)}/>
+                    <label>city:</label>
+                    <input type="text" id="city" onChange={city => setCity(city.target.value)}/>
+                    <label>house number:</label>
+                    <input type="text" id="house number"
+                           onChange={houseNumber => setHouseNumber(houseNumber.target.value)}/>
+                    <label>Zip Code:</label>
+                    <input type="text" id="zip code" onChange={zipCode => setZipCode(zipCode.target.value)}/>
+                    <label>Family Name:</label>
+                    <input type="text" id="family name"
+                           onChange={familyName => setFamilyName(familyName.target.value)}/>
+                    <label>Registration Date:</label>
+                    <input type="text" id="registration date"
+                           onChange={registrationDate => setRegistrationDate(registrationDate.target.value)}/>
+                </form>
+                <button onClick={submitAndChangeDisplay}>Register Family</button>
+            </div>
+        )
+    } else if (!display && loading === true) {
+        return (
+            <div>
+                Loading...............
+            </div>
+        )
+    }
+    else if (!display && error !== null) {
+        return (
+            <div>
+                Error...............
+            </div>
+        )
+    }
+    else {
+        return (
+            <>
+                <div>{data.familyName}</div>
+                <p>{data.familyID}</p>
+                <p>{data.adminID}</p>
+                <p>{data.registrationDate}</p>
+            </>
+        )
     }
 
 
 
 
-    //target.value -> permite que cada valor introduzido seja acrescentado ao valor anterior. Se for apagado algum dos valores, altera também o estado atual da variável.
-    // Consultar Manuel Almeida
 
-    return (
-        <div>
-            <form>
-                <label>email:</label>
-                <input type="text" id="emailID" onChange={emailID => setEmailID(emailID.target.value)}/>
-                <label>admin name:</label>
-                <input type="text" id="name" onChange={name => setName(name.target.value)}/>
-                <label>birthdate:</label>
-                <input type="text" id="birthdate" onChange={birthdate => setBirthdate(birthdate.target.value)}/>
-                <label>Vat Number:</label>
-                <input type="text" id="vatnumber" onChange={vatNumber => setVatNumber(vatNumber.target.value)}/>
-                <label>phone:</label>
-                <input type="text" id="phone" onChange={phone => setPhoneNumber(phone.target.value)}/>
-                <label>street:</label>
-                <input type="text" id="street" onChange={street => setStreet(street.target.value)}/>
-                <label>city:</label>
-                <input type="text" id="city" onChange={city => setCity(city.target.value)}/>
-                <label>house number:</label>
-                <input type="text" id="house number"
-                       onChange={houseNumber => setHouseNumber(houseNumber.target.value)}/>
-                <label>Zip Code:</label>
-                <input type="text" id="zip code" onChange={zipCode => setZipCode(zipCode.target.value)}/>
-                <label>Family Name:</label>
-                <input type="text" id="family name" onChange={familyName => setFamilyName(familyName.target.value)}/>
-                <label>Registration Date:</label>
-                <input type="text" id="registration date"
-                       onChange={registrationDate => setRegistrationDate(registrationDate.target.value)}/>
-            </form>
-            <button onClick={handleSubmit}>Register Family</button>
-        </div>
-
-    )
 }
 
 
