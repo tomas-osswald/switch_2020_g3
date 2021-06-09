df5d8ac725f959627fb4a2b5b0a7ba92ab803e18
João Pinto [1201765] <1201765@isep.ipp.pt>
2021-06-09 18:40:10 +0100

    Update to permite all requests w/o jwt

--- src/main/java/switchtwentytwenty/project/authentication/WebSecurityConfig.java
+++ src/main/java/switchtwentytwenty/project/authentication/WebSecurityConfig.java
@@ -53,12 +53,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // We don't need CSRF for this example
         httpSecurity.csrf().disable()
                 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/authenticate", "/register", "/categories").permitAll().
+                .authorizeRequests().anyRequest().permitAll()
+                //.antMatchers("/authenticate", "/register", "/categories").permitAll().
                 // all other requests need to be authenticated
-                        anyRequest().authenticated().and().
+                        //anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
                 // store user's state.
-                        exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+                        .and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         // Add a filter to validate the tokens with every request
