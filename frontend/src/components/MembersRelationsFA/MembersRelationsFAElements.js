a788793c4c599b03ba7b1595de2ccb316c22fe05
Diogo Ribeiro [1201768] <1201768@isep.ipp.pt>
2021-06-11 00:16:43 +0100

    add overflow to mainView and family

--- frontend/src/components/MembersRelationsFA/MembersRelationsFAElements.js
+++ frontend/src/components/MembersRelationsFA/MembersRelationsFAElements.js
@@ -2,22 +2,14 @@ import styled from 'styled-components';
 
 export const MembersRelationsFADiv = styled.div`
   
-    background-color: var(--backgroundGrey);;
+  background-color: var(--backgroundGrey);
+  overflow-x: hidden;
 `
 
 export const HeaderCell = styled.div`
 
   padding-left: 100px;
 
-`
-
-export const HeaderSection = styled.div`
-  width: 90%;
-  
-`
-
-export const ButtonCell = styled.div`
-  margin-left: 50px;
 `
 
 export const RelationsList = styled.td`
