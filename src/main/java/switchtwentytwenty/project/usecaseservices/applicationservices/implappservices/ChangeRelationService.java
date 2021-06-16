54602781251d7dc9ace3f4f3bc4813c00e5e247b
Miguel Loureiro [1181945] <1181945@isep.ipp.pt>
2021-06-16 15:33:08 +0100

    fixed conflicts

--- src/main/java/switchtwentytwenty/project/usecaseservices/applicationservices/implappservices/ChangeRelationService.java
+++ src/main/java/switchtwentytwenty/project/usecaseservices/applicationservices/implappservices/ChangeRelationService.java
@@ -3,14 +3,14 @@ package switchtwentytwenty.project.usecaseservices.applicationservices.implappse
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import switchtwentytwenty.project.datamodel.assemblerjpa.iassemblersjpa.IRelationDataDomainAssembler;
 import switchtwentytwenty.project.domain.aggregates.family.Family;
-import switchtwentytwenty.project.domain.valueobject.FamilyID;
-import switchtwentytwenty.project.domain.valueobject.Relation;
-import switchtwentytwenty.project.domain.valueobject.RelationDesignation;
-import switchtwentytwenty.project.domain.valueobject.RelationID;
+import switchtwentytwenty.project.domain.valueobject.*;
+import switchtwentytwenty.project.dto.assemblers.implassemblers.FamilyDTODomainAssembler;
 import switchtwentytwenty.project.dto.assemblers.implassemblers.RelationInputDTOAssembler;
 import switchtwentytwenty.project.dto.family.InputChangeRelationDTO;
 import switchtwentytwenty.project.dto.family.OutputRelationDTO;
+import switchtwentytwenty.project.interfaceadapters.implrepositories.FamilyRepository;
 import switchtwentytwenty.project.usecaseservices.applicationservices.iappservices.IChangeRelationService;
 import switchtwentytwenty.project.usecaseservices.irepositories.IFamilyRepository;
 
@@ -19,6 +19,7 @@ import switchtwentytwenty.project.usecaseservices.irepositories.IFamilyRepositor
 public class ChangeRelationService implements IChangeRelationService {
 
     private IFamilyRepository familyRepository;
+    private FamilyDTODomainAssembler familyDTODomainAssembler;
 
     @Autowired
     public ChangeRelationService(IFamilyRepository familyRepository) {
@@ -29,6 +30,8 @@ public class ChangeRelationService implements IChangeRelationService {
     public OutputRelationDTO changeRelation(InputChangeRelationDTO inputChangeRelationDTO){
 
         //create VO
+        PersonID personIDOne = familyDTODomainAssembler.personIDOneToDomain(inputChangeRelationDTO);
+        PersonID personIDTwo = familyDTODomainAssembler.personIDTwoToDomain(inputRelationDTO);
         FamilyID familyID = new FamilyID(inputChangeRelationDTO.getFamilyID());
 
         RelationID newRelationID = new RelationID(Integer.parseInt(inputChangeRelationDTO.getRelationID()));
@@ -36,6 +39,8 @@ public class ChangeRelationService implements IChangeRelationService {
 
         Family family = familyRepository.getByID(familyID);
         family.changeRelation(newRelationID, newRelationDesignation );
+        Family updatedFamily = familyRepository.add(family);
+        Relation updatedRelation = updatedFamily.getRelationByID(newRelationID);
 
         return new OutputRelationDTO();
     }
--- src/test/java/switchtwentytwenty/project/usecaseservices/applicationservices/implappservices/ChangeRelationServiceTest.java
+++ src/test/java/switchtwentytwenty/project/usecaseservices/applicationservices/implappservices/ChangeRelationServiceTest.java
@@ -1,4 +1,76 @@
+package switchtwentytwenty.project.usecaseservices.applicationservices.implappservices;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import switchtwentytwenty.project.domain.aggregates.family.Family;
+import switchtwentytwenty.project.domain.valueobject.FamilyID;
+import switchtwentytwenty.project.domain.valueobject.Relation;
+import switchtwentytwenty.project.domain.valueobject.RelationDesignation;
+import switchtwentytwenty.project.domain.valueobject.RelationID;
+import switchtwentytwenty.project.dto.assemblers.implassemblers.RelationInputDTOAssembler;
+import switchtwentytwenty.project.dto.family.ChangeRelationDTO;
+import switchtwentytwenty.project.dto.family.InputChangeRelationDTO;
+import switchtwentytwenty.project.dto.family.OutputRelationDTO;
+import switchtwentytwenty.project.usecaseservices.irepositories.IFamilyRepository;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(MockitoExtension.class)
 class ChangeRelationServiceTest {
-  
+
+    @Mock
+    IFamilyRepository familyRepository;
+
+    @Mock
+    RelationInputDTOAssembler relationInputDTOAssembler;
+
+    @Mock
+    Family family;
+
+    @Mock
+    Relation newRelation;
+
+    @Mock
+    OutputRelationDTO outputRelationDTO;
+
+    @InjectMocks
+    ChangeRelationService changeRelationService;
+
+    String relationID = "4";
+    String designationString = "BFF";
+    String familyIDString = "@tonyze@latinlover.com";
+    FamilyID familyID = new FamilyID(familyIDString);
+    ChangeRelationDTO changeRelationDTO = new ChangeRelationDTO(relationID, designationString);
+    InputChangeRelationDTO inputChangeRelationDTO = new InputChangeRelationDTO(changeRelationDTO, familyID.toString());
+
+    @DisplayName("Change relation service: success when changing a existent relation")
+    @Test
+    void changeRelationSuccess() {
+
+        Mockito.when(relationInputDTOAssembler.toInputChangeRelationDTO(any(ChangeRelationDTO.class), any(String.class))).thenReturn(new InputChangeRelationDTO(changeRelationDTO,familyID.toString()));
+        Mockito.when(familyRepository.getByID(any(FamilyID.class))).thenReturn(family);
+        Mockito.when(family.changeRelation(any(RelationID.class), any(RelationDesignation.class))).thenReturn();
+
+        assertDoesNotThrow(() -> changeRelationService.changeRelation(inputChangeRelationDTO));
+
+    }
+
+    @DisplayName("Change relation service: failure when changing a non-existent relation")
+    @Test
+    void changeRelationFailure() {
+
+        Mockito.when(relationInputDTOAssembler.toInputChangeRelationDTO(any(ChangeRelationDTO.class), any(String.class))).thenReturn(new InputChangeRelationDTO(changeRelationDTO,familyID.toString()));
+        Mockito.when(familyRepository.getByID(any(FamilyID.class))).thenReturn(family);
+
+
+        assertDoesNotThrow(() -> changeRelationService.changeRelation(inputChangeRelationDTO));
+
+    }
+
 }
