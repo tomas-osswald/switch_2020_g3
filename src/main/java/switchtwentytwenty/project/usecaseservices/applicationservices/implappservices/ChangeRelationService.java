c0f78766c8476607ccaa90e0dbd36ab823fae9dc
Miguel Loureiro [1181945] <1181945@isep.ipp.pt>
2021-06-16 12:03:58 +0100

    initated Change relation service

--- src/main/java/switchtwentytwenty/project/usecaseservices/applicationservices/implappservices/ChangeRelationService.java
+++ src/main/java/switchtwentytwenty/project/usecaseservices/applicationservices/implappservices/ChangeRelationService.java
@@ -1,21 +1,43 @@
 package switchtwentytwenty.project.usecaseservices.applicationservices.implappservices;
 
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import switchtwentytwenty.project.domain.aggregates.family.Family;
+import switchtwentytwenty.project.domain.valueobject.FamilyID;
 import switchtwentytwenty.project.domain.valueobject.Relation;
+import switchtwentytwenty.project.domain.valueobject.RelationDesignation;
+import switchtwentytwenty.project.domain.valueobject.RelationID;
+import switchtwentytwenty.project.dto.assemblers.implassemblers.RelationInputDTOAssembler;
 import switchtwentytwenty.project.dto.family.InputChangeRelationDTO;
+import switchtwentytwenty.project.dto.family.OutputRelationDTO;
 import switchtwentytwenty.project.usecaseservices.applicationservices.iappservices.IChangeRelationService;
+import switchtwentytwenty.project.usecaseservices.irepositories.IFamilyRepository;
 
 @Service
 @NoArgsConstructor
 public class ChangeRelationService implements IChangeRelationService {
 
+    private IFamilyRepository familyRepository;
+
+    @Autowired
+    public ChangeRelationService(IFamilyRepository familyRepository) {
+        this.familyRepository = familyRepository;
+    }
 
 
-    public Relation changeRelation(InputChangeRelationDTO inputChangeRelationDTO){
+    public OutputRelationDTO changeRelation(InputChangeRelationDTO inputChangeRelationDTO){
 
+        //create VO
+        FamilyID familyID = new FamilyID(inputChangeRelationDTO.getFamilyID());
 
-        throw new UnsupportedOperationException();
+        RelationID newRelationID = new RelationID(Integer.parseInt(inputChangeRelationDTO.getRelationID()));
+        RelationDesignation newRelationDesignation = new RelationDesignation(inputChangeRelationDTO.getNewDesignation());
+
+        Family family = familyRepository.getByID(familyID);
+        family.changeRelation(newRelationID, newRelationDesignation );
+
+        return new OutputRelationDTO();
     }
 
 
