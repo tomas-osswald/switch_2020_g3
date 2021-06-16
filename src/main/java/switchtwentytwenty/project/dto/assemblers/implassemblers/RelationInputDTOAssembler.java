fe9c3cb115f849f3bee5097887d482851cc4711b
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-06-16 10:36:32 +0100

    created ChangeRelationDTO, InputChangeRelationDTO, and method in the RelationInputDTOAssembler

--- src/main/java/switchtwentytwenty/project/dto/assemblers/implassemblers/RelationInputDTOAssembler.java
+++ src/main/java/switchtwentytwenty/project/dto/assemblers/implassemblers/RelationInputDTOAssembler.java
@@ -2,7 +2,9 @@ package switchtwentytwenty.project.dto.assemblers.implassemblers;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+import switchtwentytwenty.project.dto.family.ChangeRelationDTO;
 import switchtwentytwenty.project.dto.family.CreateRelationDTO;
+import switchtwentytwenty.project.dto.family.InputChangeRelationDTO;
 import switchtwentytwenty.project.dto.family.InputRelationDTO;
 
 @Component
@@ -10,8 +12,11 @@ import switchtwentytwenty.project.dto.family.InputRelationDTO;
 public class RelationInputDTOAssembler {
 
 
-
     public InputRelationDTO toInputRelationDTO(CreateRelationDTO createRelationDTO, String familyID) {
         return new InputRelationDTO(createRelationDTO.getMemberOneID(), createRelationDTO.getMemberTwoID(), createRelationDTO.getRelationDesignation(), familyID);
+    }
+
+    public InputChangeRelationDTO inputChangeRelationDTO(ChangeRelationDTO changeRelationDTO, String familyID) {
+        return new InputChangeRelationDTO(changeRelationDTO, familyID);
     }
 }
---
+++ src/main/java/switchtwentytwenty/project/dto/family/InputChangeRelationDTO.java
@@ -0,0 +1,39 @@
+package switchtwentytwenty.project.dto.family;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class InputChangeRelationDTO {
+
+    private String relationID;
+    private String newDesignation;
+    private String familyID;
+
+    public InputChangeRelationDTO(ChangeRelationDTO changeRelationDTO, String familyID) {
+        this.relationID = changeRelationDTO.getRelationID();
+        this.newDesignation = changeRelationDTO.getNewRelationDesignation();
+        this.familyID = familyID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InputChangeRelationDTO that = (InputChangeRelationDTO) o;
+        return Objects.equals(relationID, that.relationID) && Objects.equals(newDesignation, that.newDesignation) && Objects.equals(familyID, that.familyID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(relationID, newDesignation, familyID);
+    }
+
+}
