cb8d3411ebc5d5ffb53a7114e62a0e5fda69c839
Tomás Osswald [1201784] <1201784@isep.ipp.pt>
2021-06-17 16:24:17 +0100

    add SecurityUserTest class

---
+++ src/test/java/switchtwentytwenty/project/authentication/SecurityUserTest.java
@@ -0,0 +1,49 @@
+package switchtwentytwenty.project.authentication;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SecurityUserTest {
+
+    String username = "username";
+    String password = "password";
+    String role = "role";
+    SecurityUser securityUser = new SecurityUser(username, password, role);
+
+    @Test
+    void getAuthorities() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void isAccountNonExpired() {
+    }
+
+    @Test
+    void isAccountNonLocked() {
+    }
+
+    @Test
+    void isCredentialsNonExpired() {
+    }
+
+    @Test
+    void isEnabled() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
