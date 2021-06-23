fb86233d88976dbd1dc21a6a694134d43f11f191
Ricardo Nogueira [1201780] <1201780@isep.ipp.pt>
2021-06-23 09:39:12 +0100

    fixed test

--- src/test/java/switchtwentytwenty/project/interfaceadapters/controller/implcontrollers/FamilyRESTControllerIT.java
+++ src/test/java/switchtwentytwenty/project/interfaceadapters/controller/implcontrollers/FamilyRESTControllerIT.java
@@ -1,13 +1,11 @@
 package switchtwentytwenty.project.interfaceadapters.controller.implcontrollers;
 
-import io.jsonwebtoken.Jwt;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.hateoas.Link;
@@ -342,7 +340,7 @@ class FamilyRESTControllerIT {
     @DisplayName("Family Members And Relations IT - Failure - Not Authorized")
     @Test
     void getFamilyMembersAndRelationsITFailureNotAuthorized() {
-        String invalidJWT = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b255emVAbGF0aW5sb3Zlci5jb20iLCJyb2xlIjoiZmFtaWx5QWRtaW5pc3RyYXRvciIsImV4cCI6MTYyNDM4MDU4NSwiaWF0IjoxNjI0MzYyNTg1fQ.enz1EEVmaITp0KIGDLXqvLvPO3wkfhnwbbXVxH9xFTOOM3PTZUdh60fBkcIjM-CYIO5k04b0qCXMugHkYp9kjQ";
+        String invalidJWT = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b255emVAbGF0aW5sb3Zlci5jb20iLCJyb2xlIjoiZmFtaWx5QWRtaW5pc3RyYXRvciIsImV4cCI6MTgwMDAwMTYyNDI5MjcxMiwiaWF0IjoxNjI0MjkyNzEyfQ.L0ib9t84dubgRWdtK_WCtHBMagp3QbDcNlcLLACPqSRFt3__sJ0T7ef5tGEARj-aRuGXZdxOhMpOG39BwS6KRg";
 
         PersonRepository personRepository = new PersonRepository(iPersonRepositoryJPA, personDataDomainAssembler);
         FamilyRepository familyRepository = new FamilyRepository(iFamilyRepositoryJPA, familyDataDomainAssembler);
