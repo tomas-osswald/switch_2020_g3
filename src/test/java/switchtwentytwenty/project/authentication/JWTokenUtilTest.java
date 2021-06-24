d27cf0bffdfe07cdd385e3837c6663a9090726c7
Bruno Tamames [1201762] <1201762@isep.ipp.pt>
2021-06-24 11:28:22 +0100

    added basic tests to uncovered methods

--- src/test/java/switchtwentytwenty/project/authentication/JWTokenUtilTest.java
+++ src/test/java/switchtwentytwenty/project/authentication/JWTokenUtilTest.java
@@ -42,4 +42,18 @@ class JWTokenUtilTest {
         assertEquals(jwTokenUtil.getUsernameFromToken(result), username);
     }
 
+    @Test
+    void validateTokenTestWrongUser(){
+        DAOUser daoUser = new DAOUser();
+        daoUser.setUsername("rifens@ravens.com");
+        daoUser.setPassword(password);
+        daoUser.setRole(role);
+
+        UserDetails userDetails = new SecurityUser(daoUser.getUsername(), daoUser.getPassword(), daoUser.getRole());
+
+        String result = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b255emVAbGF0aW5sb3Zlci5jb20iLCJyb2xlIjoiZmFtaWx5QWRtaW5pc3RyYXRvciIsImV4cCI6MTgwMDAwMTYyNDI5MjcxMiwiaWF0IjoxNjI0MjkyNzEyfQ.L0ib9t84dubgRWdtK_WCtHBMagp3QbDcNlcLLACPqSRFt3__sJ0T7ef5tGEARj-aRuGXZdxOhMpOG39BwS6KRg";
+
+        assertFalse(jwTokenUtil.validateToken(result, userDetails));
+    }
+
 }
